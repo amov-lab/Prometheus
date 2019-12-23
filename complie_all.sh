e19a6a6d6b7044d11c441474fca2b5c64fec8bca
Qi-Thinkpad-laptop <fatmoonqyp@126.com>
2019-12-23 12:10:43 +0800

    msgs:add a new msg target_from_vision.msg

--- complie_all.sh
+++ complie_all.sh
@@ -1,2 +1,3 @@
+catkin_make --source Modules/msgs --build build/msgs
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/object_detection --build build/object_detection
