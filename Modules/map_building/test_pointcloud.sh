5c541d6b7f3214a0b1eeae1b6df36a92d5c28916
Lee0326 <lcyfly1@163.com>
2019-12-26 09:43:28 +0800

    New Feature: new sh file

--- Modules/map_building/test_pointcloud.sh
+++ Modules/map_building/test_pointcloud.sh
@@ -2,7 +2,7 @@ gnome-terminal -x bash -c "source /opt/ros/kinetic/setup.bash && roscore"
 
 sleep 3
 
-gnome-terminal -x bash -c "cd ~/maplab_original_ws/ && source devel/setup.bash && rosrun rovioli tutorial_sineva_localization /home/colin/datasets/test_results_2.0/d1000-sineva/d1000-imu /home/colin/datasets/Localization_maps_maplab/d1000-maplab" &
+gnome-terminal -x bash -c "cd ~/maplab_original_ws/ && source devel/setup.bash && rosrun rovioli tutorial_sineva_live /home/colin/datasets/Localization_maps_maplab/d1000-maplab" &
 
 gnome-terminal -x bash -c "cd ~/code_repos/Prometheus/Modules/map_building/ && source devel/setup.bash && rosrun sineva_stereo sineva_stereo_node"
 
