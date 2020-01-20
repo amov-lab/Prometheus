3fee72a70582043114bf12ceb21ef38bef47f492
qi-ubuntu-desktop <fatmoonqyp@126.com>
2020-01-20 15:32:42 +0800

    simulator

--- Simulator/gazebo_simulator/scripts/qrcode_control.py
+++ Simulator/gazebo_simulator/scripts/qrcode_control.py
@@ -12,13 +12,15 @@ def pose_publisher():
     pose_msg = ModelState()
     pose_msg.model_name = 'qrcode_cube'
     rate = rospy.Rate(30)
-    radius = 0.5
+    radius = 0.2
     theta = 0.
     while not rospy.is_shutdown():
-           theta += (30./30)%360
+           theta += (30./30)%2000
            pose_msg.pose.position.x = radius*math.cos(math.radians(theta))
            pose_msg.pose.position.y = radius*math.sin(math.radians(theta))
            pub.publish(pose_msg)
+           print('Pos_x :',pose_msg.pose.position.x)
+           print('Pos_y :',pose_msg.pose.position.y)
            rate.sleep()
 
 if __name__ == '__main__':
