38c95df9ec297fe51915c257473d6cc043982c7b
qi-ubuntu-desktop <fatmoonqyp@126.com>
2020-03-13 01:15:21 +0800

    correct the complie.sh

--- complie_all.sh
+++ complie_all.sh
@@ -3,3 +3,4 @@ catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/object_detection --build build/object_detection
 catkin_make --source Modules/mission --build build/mission
 catkin_make --source Modules/planning --build build/planning
+catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
