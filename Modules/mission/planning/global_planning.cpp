7e1c5deec1e7cf829bfa3cc454e6b78c29236181
qi-ubuntu-desktop <fatmoonqyp@126.com>
2020-03-18 23:24:15 +0800

    control: fix small bugs

--- Modules/mission/planning/global_planning.cpp
+++ Modules/mission/planning/global_planning.cpp
@@ -26,22 +26,17 @@ nav_msgs::Path path;
 int Num_total_wp;
 int wp_id;
 
-
 prometheus_msgs::PositionReference pos_cmd;
 prometheus_msgs::ControlCommand Command_Now;                               //发送给控制模块 [px4_pos_controller.cpp]的命令
-ros::Publisher odom_pub;
-ros::Publisher trajectory_pub;
-ros::Publisher drone_pub;
-prometheus_msgs::DroneState _DroneState;                                   //无人机状态量
+
 ros::Publisher command_pub;
 int flag_get_cmd = 0;
 geometry_msgs::PoseStamped goal;
-geometry_msgs::PoseStamped drone_pos;
+prometheus_msgs::DroneState _DroneState;
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>声 明 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 int posehistory_window_ = 1000;
 std::vector<geometry_msgs::PoseStamped> posehistory_vector_;
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>回 调 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
 void path_cb(const nav_msgs::Path::ConstPtr& msg)
 {
     flag_get_cmd = 1;
@@ -50,54 +45,10 @@ void path_cb(const nav_msgs::Path::ConstPtr& msg)
     wp_id = 0;
     cout << "Get a new path!"<<endl;
 }
-
 void drone_state_cb(const prometheus_msgs::DroneState::ConstPtr& msg)
 {
     _DroneState = *msg;
-    nav_msgs::Odometry Odom_Now;
-    Odom_Now.header.stamp = ros::Time::now();
-    Odom_Now.header.frame_id = "map";
-
-    Odom_Now.pose.pose.position.x = _DroneState.position[0];
-    Odom_Now.pose.pose.position.y = _DroneState.position[1];
-    Odom_Now.pose.pose.position.z = _DroneState.position[2];
-
-    if(Odom_Now.pose.pose.position.z <= 0)
-    {
-        Odom_Now.pose.pose.position.z = 0.01;
-    }
-
-//  Odom_Now.pose.pose.orientation = geometry_msgs::Quaternion(_DroneState.attitude[2],_DroneState.attitude[1], _DroneState.attitude[0]); // yaw, pitch, roll
-    Odom_Now.pose.pose.orientation = _DroneState.attitude_q;
-    Odom_Now.twist.twist.linear.x = _DroneState.velocity[0];
-    Odom_Now.twist.twist.linear.y = _DroneState.velocity[1];
-    Odom_Now.twist.twist.linear.z = _DroneState.velocity[2];
-    odom_pub.publish(Odom_Now);
-
-    
-    drone_pos.header.stamp = ros::Time::now();
-    drone_pos.header.frame_id = "map";
-    drone_pos.pose.position.x = _DroneState.position[0];
-    drone_pos.pose.position.y = _DroneState.position[1];
-    drone_pos.pose.position.z = _DroneState.position[2];
-
-    drone_pos.pose.orientation = _DroneState.attitude_q;
-    drone_pub.publish(drone_pos);
-
-    //发布无人机的位姿 和 轨迹 用作rviz中显示
-    posehistory_vector_.insert(posehistory_vector_.begin(), drone_pos);
-    if(posehistory_vector_.size() > posehistory_window_){
-        posehistory_vector_.pop_back();
-    }
-    
-    nav_msgs::Path drone_trajectory;
-    drone_trajectory.header.stamp = ros::Time::now();
-    drone_trajectory.header.frame_id = "map";
-    drone_trajectory.poses = posehistory_vector_;
-    trajectory_pub.publish(drone_trajectory);
-
 }
-
 void goal_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
     goal = *msg;
@@ -116,12 +67,6 @@ int main(int argc, char **argv)
 
     //【订阅】无人机当前状态
     ros::Subscriber drone_state_sub = nh.subscribe<prometheus_msgs::DroneState>("/prometheus/drone_state", 10, drone_state_cb);
-    
-    //【发布】无人机odometry
-    odom_pub = nh.advertise<nav_msgs::Odometry>("/planning/odom_world", 10);
-
-    trajectory_pub = nh.advertise<nav_msgs::Path>("/planning/drone_trajectory", 10);
-    drone_pub = nh.advertise<geometry_msgs::PoseStamped>("/planning/drone_pose", 10);
 
     // 频率 [10Hz]
     ros::Rate rate(10.0);
@@ -217,11 +162,7 @@ int main(int argc, char **argv)
         }
         
         cout <<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>Planning<<<<<<<<<<<<<<<<<<<<<<<<<" <<endl;
-        cout << "drone_pos: " << drone_pos.pose.position.x << " [m] "<< drone_pos.pose.position.y << " [m] "<< drone_pos.pose.position.z << " [m] "<<endl;
         cout << "goal_pos: " << goal.pose.position.x << " [m] "<< goal.pose.position.y << " [m] "<< goal.pose.position.z << " [m] "<<endl;
-
-        
-
 
         rate.sleep();
     }
