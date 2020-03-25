865195e6adf947ee4987eeb7a43802a56d1ea5c4
qi-ubuntu-desktop <fatmoonqyp@126.com>
2020-03-25 12:20:19 +0800

    simulator: add pcl ground truth

--- Modules/slam/src/map_building/pointcloud2_publisher.cpp
+++ Modules/slam/src/map_building/pointcloud2_publisher.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -8,11 +9,22 @@
 main(int argc, char **argv)
 {
     ros::init(argc, argv, "pc2_publisher");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
     ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("prometheus/pcl_groundtruth", 1);
     pcl::PointCloud<pcl::PointXYZ> cloud;
     sensor_msgs::PointCloud2 output;
-    pcl::io::loadPCDFile("/home/colin/Desktop/obstacle.pcd", cloud);
+
+
+    std::string pcd_path;
+    if (nh.getParam("pcd_path", pcd_path)) {
+        ROS_INFO("Get the pcd_path : %s", pcd_path.c_str());
+    } else {
+        ROS_WARN("didn't find parameter pcd_path, use the default path");
+        //std::string ros_path = ros::package::getPath("prometheus_gazebo");
+        pcd_path = "/home/fly-vision/Prometheus/Simulator/gazebo_simulator/maps/obstacle.pcd";
+    }
+
+    pcl::io::loadPCDFile(pcd_path, cloud);
     //Convert the cloud to ROS message
     pcl::toROSMsg(cloud, output);
     output.header.frame_id = "world"; //this has been done in order to be able to visualize our PointCloud2 message on the RViz visualizer
--- complie_all.sh
+++ complie_all.sh
@@ -2,5 +2,6 @@ catkin_make --source Modules/msgs --build build/msgs
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/object_detection --build build/object_detection
 catkin_make --source Modules/mission --build build/mission
+catkin_make --source Modules/slam --build build/slam
 catkin_make --source Modules/planning --build build/planning
 catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
