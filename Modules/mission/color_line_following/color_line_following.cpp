de59488a3c97d9ae9f752a05bf4e223ffb7570bd
Yuhua <fatmoonqyp@126.com>
2020-04-08 01:53:15 +0800

    mission: color_following.cpp

--- Modules/mission/color_line_following/color_line_following.cpp
+++ Modules/mission/color_line_following/color_line_following.cpp
@@ -14,9 +14,10 @@
 #include <tf/transform_datatypes.h>
 //topic 头文件
 #include <prometheus_msgs/DroneState.h>
-#include <std_msgs/Float32.h>
+#include <geometry_msgs/Pose.h>
 #include <prometheus_msgs/ControlCommand.h>
 #include <nav_msgs/Odometry.h>
+
 using namespace std;
 using namespace Eigen;
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>全 局 变 量<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -27,17 +28,28 @@ float start_point_x,start_point_y,start_point_z;
 float velocity;
 float k_p;
 //---------------------------------------Vision---------------------------------------------
-std_msgs::Float32 color_line_angle;         
-
+int flag_detection;
+float error_body_y;
+float yaw_sp;
 //---------------------------------------Output---------------------------------------------
 prometheus_msgs::ControlCommand Command_Now;                               //发送给控制模块 [px4_pos_controller.cpp]的命令
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>函数声明<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 void printf_param();                                                                 //打印各项参数以供检查
 void printf_result();
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>回调函数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-void vision_cb(const std_msgs::Float32::ConstPtr &msg)
+void vision_cb(const geometry_msgs::Pose::ConstPtr &msg)
 {
-    color_line_angle = *msg;
+    error_body_y = - tan(msg->position.x) * start_point_z;
+    flag_detection = msg->position.y;
+
+    float x1 = msg->orientation.w;
+    float y1 = msg->orientation.x;
+    float x2 = msg->orientation.y;
+    float y2 = msg->orientation.z;
+
+    float next_desired_yaw = - atan2(y2 - y1, x2 - x1);
+
+    yaw_sp = (0.7*yaw_sp + 0.3*next_desired_yaw);
 }
 void drone_state_cb(const prometheus_msgs::DroneState::ConstPtr& msg)
 {
@@ -54,7 +66,7 @@ int main(int argc, char **argv)
     ros::Rate rate(20.0);
 
     //【订阅】所识别线与相机中心轴的偏移角度
-    ros::Subscriber vision_sub = nh.subscribe<std_msgs::Float32>("/prometheus/vision/color_line_angle", 10, vision_cb);
+    ros::Subscriber vision_sub = nh.subscribe<geometry_msgs::Pose>("/prometheus/vision/color_line_angle", 10, vision_cb);
 
     ros::Subscriber drone_state_sub = nh.subscribe<prometheus_msgs::DroneState>("/prometheus/drone_state", 10, drone_state_cb);
 
@@ -118,6 +130,7 @@ int main(int argc, char **argv)
         ros::spinOnce();
     }
 
+    ros::Duration(5.0).sleep();
 
     // 先读取一些飞控的数据
     for(int i=0;i<10;i++)
@@ -136,17 +149,17 @@ int main(int argc, char **argv)
 
         Command_Now.Mode = prometheus_msgs::ControlCommand::Move;
         Command_Now.Reference_State.Move_mode           = prometheus_msgs::PositionReference::XY_VEL_Z_POS;
-        Command_Now.Reference_State.Move_frame          = prometheus_msgs::PositionReference::BODY_FRAME;
+        Command_Now.Reference_State.Move_frame          = prometheus_msgs::PositionReference::MIX_FRAME;
         Command_Now.Reference_State.velocity_ref[0]     = velocity;
-        Command_Now.Reference_State.velocity_ref[1]     = 0.0;
+        Command_Now.Reference_State.velocity_ref[1]     = k_p * error_body_y;
         Command_Now.Reference_State.position_ref[2]     = start_point_z;
-        Command_Now.Reference_State.yaw_ref             = k_p * color_line_angle.data * 0.05;
+        Command_Now.Reference_State.yaw_ref             = yaw_sp;
 
         
         //Publish
         Command_Now.header.stamp = ros::Time::now();
         Command_Now.Command_ID   = Command_Now.Command_ID + 1;
-        //command_pub.publish(Command_Now);
+        command_pub.publish(Command_Now);
         printf_result();
         rate.sleep();
 
@@ -159,9 +172,8 @@ int main(int argc, char **argv)
 void printf_result()
 {
     cout << ">>>>>>>>>>>>>>>>>>>>>>>>Color Line Following Mission<<<<<<<<<<<<<<<<<<<< "<< endl;
-    
-    cout << "color_line_angle: " << color_line_angle.data <<endl;
-    cout << "yaw_ref: " << Command_Now.Reference_State.yaw_ref/3.1415926 *180 << " [deg] "<<endl;
+    cout << "error_body_y: " << error_body_y << " [m] "<<endl;
+    cout << "yaw_sp: " << yaw_sp/3.1415926 *180 << " [deg] "<<endl;
 }
 void printf_param()
 {
