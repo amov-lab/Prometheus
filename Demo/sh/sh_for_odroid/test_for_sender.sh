b90e8b9329eabbcf942dd081fa43d9cc832039ed
Yuhua <fatmoonqyp@126.com>
2020-04-28 17:37:58 +0800

    add launch for experiment

--- Demo/sh/sh_for_odroid/test_for_sender.sh
+++
@@ -1,6 +0,0 @@
-##
-mate-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 3; roslaunch vrpn_client_ros sample.launch server:=192.168.1.62; exec bash"' \
---tab -e 'bash -c "sleep 4; roslaunch mavros px4.launch fcu_url:="/dev/ttyUSB0:921600"; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch prometheus_control px4_pos_estimator.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch prometheus_control px4_sender.launch; exec bash"' \
--- Demo/sh/test/test_for_autonomous_landing.sh
+++
@@ -1,9 +0,0 @@
-##
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 2; rosrun prometheus_control fake_vicon; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch mavros px4.launch fcu_url:="/dev/ttyTHS2:921600"; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch prometheus_control px4_pos_estimator.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch prometheus_control px4_pos_controller.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch landpad_det landpad_det.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch prometheus_control autonomous_landing.launch; exec bash"' \
-
