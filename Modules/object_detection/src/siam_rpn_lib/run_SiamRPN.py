60f415ac9b2ca6c69b2e82bfaf23959772f2a53e
jario <jariof@foxmail.com>
2020-05-18 21:42:35 +0800

    detection: add new tracking algorithm (siamRPN)

---
+++ Modules/object_detection/src/siam_rpn_lib/run_SiamRPN.py
@@ -0,0 +1,188 @@
+import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
+import cv2
+
+from utils import get_subwindow_tracking
+
+
+def generate_anchor(total_stride, scales, ratios, score_size):
+    anchor_num = len(ratios) * len(scales)
+    anchor = np.zeros((anchor_num, 4),  dtype=np.float32)
+    size = total_stride * total_stride
+    count = 0
+    for ratio in ratios:
+        # ws = int(np.sqrt(size * 1.0 / ratio))
+        ws = int(np.sqrt(size / ratio))
+        hs = int(ws * ratio)
+        for scale in scales:
+            wws = ws * scale
+            hhs = hs * scale
+            anchor[count, 0] = 0
+            anchor[count, 1] = 0
+            anchor[count, 2] = wws
+            anchor[count, 3] = hhs
+            count += 1
+
+    anchor = np.tile(anchor, score_size * score_size).reshape((-1, 4))
+    ori = - (score_size / 2) * total_stride
+    xx, yy = np.meshgrid([ori + total_stride * dx for dx in range(score_size)],
+                         [ori + total_stride * dy for dy in range(score_size)])
+    xx, yy = np.tile(xx.flatten(), (anchor_num, 1)).flatten(), \
+             np.tile(yy.flatten(), (anchor_num, 1)).flatten()
+    anchor[:, 0], anchor[:, 1] = xx.astype(np.float32), yy.astype(np.float32)
+    return anchor
+
+
+class TrackerConfig(object):
+    # These are the default hyper-params for DaSiamRPN 0.3827
+    windowing = 'cosine'  # to penalize large displacements [cosine/uniform]
+    # Params from the network architecture, have to be consistent with the training
+    exemplar_size = 127  # input z size
+    instance_size = 271  # input x size (search region)
+    total_stride = 8
+    score_size = (instance_size-exemplar_size)/total_stride+1
+    context_amount = 0.5  # context amount for the exemplar
+    ratios = [0.33, 0.5, 1, 2, 3]
+    scales = [8, ]
+    anchor_num = len(ratios) * len(scales)
+    anchor = []
+    penalty_k = 0.055
+    window_influence = 0.42
+    lr = 0.295
+    # adaptive change search region #
+    adaptive = True
+
+    def update(self, cfg):
+        for k, v in cfg.items():
+            setattr(self, k, v)
+        self.score_size = (self.instance_size - self.exemplar_size) / self.total_stride + 1
+
+
+def tracker_eval(net, x_crop, target_pos, target_sz, window, scale_z, p):
+    
+    delta, score = net(x_crop)
+    
+    delta = delta.permute(1, 2, 3, 0).contiguous().view(4, -1).data.cpu().numpy()
+    score = F.softmax(score.permute(1, 2, 3, 0).contiguous().view(2, -1), dim=0).data[1, :].cpu().numpy()
+
+    delta[0, :] = delta[0, :] * p.anchor[:, 2] + p.anchor[:, 0]
+    delta[1, :] = delta[1, :] * p.anchor[:, 3] + p.anchor[:, 1]
+    delta[2, :] = np.exp(delta[2, :]) * p.anchor[:, 2]
+    delta[3, :] = np.exp(delta[3, :]) * p.anchor[:, 3]
+
+    def change(r):
+        return np.maximum(r, 1./r)
+
+    def sz(w, h):
+        pad = (w + h) * 0.5
+        sz2 = (w + pad) * (h + pad)
+        return np.sqrt(sz2)
+
+    def sz_wh(wh):
+        pad = (wh[0] + wh[1]) * 0.5
+        sz2 = (wh[0] + pad) * (wh[1] + pad)
+        return np.sqrt(sz2)
+
+    # size penalty
+    s_c = change(sz(delta[2, :], delta[3, :]) / (sz_wh(target_sz)))  # scale penalty
+    r_c = change((target_sz[0] / target_sz[1]) / (delta[2, :] / delta[3, :]))  # ratio penalty
+
+    penalty = np.exp(-(r_c * s_c - 1.) * p.penalty_k)
+    pscore = penalty * score
+
+    # window float
+    pscore = pscore * (1 - p.window_influence) + window * p.window_influence
+    
+    best_pscore_id = np.argmax(pscore)
+
+    target = delta[:, best_pscore_id] / scale_z
+    target_sz = target_sz / scale_z
+    lr = penalty[best_pscore_id] * score[best_pscore_id] * p.lr
+    
+    if np.max(score)<0.3:
+        target[0] = 0
+        target[1] = 0
+
+    res_x = target[0] + target_pos[0]
+    res_y = target[1] + target_pos[1]
+
+    res_w = target_sz[0] * (1 - lr) + target[2] * lr
+    res_h = target_sz[1] * (1 - lr) + target[3] * lr
+
+    target_pos = np.array([res_x, res_y])
+    target_sz = np.array([res_w, res_h])
+    return target_pos, target_sz, score[best_pscore_id]
+
+
+def SiamRPN_init(im, target_pos, target_sz, net):
+    state = dict()
+    p = TrackerConfig()
+    p.update(net.cfg)
+    state['im_h'] = im.shape[0]
+    state['im_w'] = im.shape[1]
+
+    if p.adaptive:
+        if ((target_sz[0] * target_sz[1]) / float(state['im_h'] * state['im_w'])) < 0.004:
+            p.instance_size = 287  # small object big search region
+        else:
+            p.instance_size = 271
+
+        p.score_size = (p.instance_size - p.exemplar_size) / p.total_stride + 1
+
+    p.anchor = generate_anchor(p.total_stride, p.scales, p.ratios, int(p.score_size))
+
+    avg_chans = np.mean(im, axis=(0, 1))
+
+    wc_z = target_sz[0] + p.context_amount * sum(target_sz)
+    hc_z = target_sz[1] + p.context_amount * sum(target_sz)
+    s_z = round(np.sqrt(wc_z * hc_z))
+    # initialize the exemplar
+    z_crop = get_subwindow_tracking(im, target_pos, p.exemplar_size, s_z, avg_chans)
+
+    z = Variable(z_crop.unsqueeze(0))
+    net.temple(z.cuda())
+
+    if p.windowing == 'cosine':
+        window = np.outer(np.hanning(p.score_size), np.hanning(p.score_size))
+    elif p.windowing == 'uniform':
+        window = np.ones((p.score_size, p.score_size))
+    window = np.tile(window.flatten(), p.anchor_num)
+
+    state['p'] = p
+    state['net'] = net
+    state['avg_chans'] = avg_chans
+    state['window'] = window
+    state['target_pos'] = target_pos
+    state['target_sz'] = target_sz
+    return state
+
+
+def SiamRPN_track(state, im):
+    p = state['p']
+    net = state['net']
+    avg_chans = state['avg_chans']
+    window = state['window']
+    target_pos = state['target_pos']
+    target_sz = state['target_sz']
+
+    wc_z = target_sz[1] + p.context_amount * sum(target_sz)
+    hc_z = target_sz[0] + p.context_amount * sum(target_sz)
+    s_z = np.sqrt(wc_z * hc_z)
+    scale_z = p.exemplar_size / s_z
+    d_search = (p.instance_size - p.exemplar_size) / 2
+    pad = d_search / scale_z
+    s_x = s_z + 2 * pad
+
+    # extract scaled crops for search region x at previous target position
+    x_crop = Variable(get_subwindow_tracking(im, target_pos, p.instance_size, round(s_x), avg_chans).unsqueeze(0))
+
+    target_pos, target_sz, score = tracker_eval(net, x_crop.cuda(), target_pos, target_sz * scale_z, window, scale_z, p)
+    target_pos[0] = max(0, min(state['im_w'], target_pos[0]))
+    target_pos[1] = max(0, min(state['im_h'], target_pos[1]))
+    target_sz[0] = max(10, min(state['im_w'], target_sz[0]))
+    target_sz[1] = max(10, min(state['im_h'], target_sz[1]))
+    state['target_pos'] = target_pos
+    state['target_sz'] = target_sz
+    state['score'] = score
+    return state
---
+++ Modules/object_detection/src/siam_rpn_lib/utils.py
@@ -0,0 +1,140 @@
+import cv2
+import torch
+import numpy as np
+
+def generate_anchor(total_stride, scales, ratios, score_size):
+    anchor_num = len(ratios) * len(scales)
+    anchor = np.zeros((anchor_num, 4),  dtype=np.float32)
+    size = total_stride * total_stride
+    count = 0
+    for ratio in ratios:
+        # ws = int(np.sqrt(size * 1.0 / ratio))
+        ws = int(np.sqrt(size / ratio))
+        hs = int(ws * ratio)
+        for scale in scales:
+            wws = ws * scale
+            hhs = hs * scale
+            anchor[count, 0] = 0
+            anchor[count, 1] = 0
+            anchor[count, 2] = wws
+            anchor[count, 3] = hhs
+            count += 1
+
+    anchor = np.tile(anchor, score_size * score_size).reshape((-1, 4))
+    ori = - (score_size / 2) * total_stride
+    xx, yy = np.meshgrid([ori + total_stride * dx for dx in range(score_size)],
+                         [ori + total_stride * dy for dy in range(score_size)])
+    xx, yy = np.tile(xx.flatten(), (anchor_num, 1)).flatten(), \
+             np.tile(yy.flatten(), (anchor_num, 1)).flatten()
+    anchor[:, 0], anchor[:, 1] = xx.astype(np.float32), yy.astype(np.float32)
+    return anchor
+
+
+def to_numpy(tensor):
+    if torch.is_tensor(tensor):
+        return tensor.cpu().numpy()
+    elif type(tensor).__module__ != 'numpy':
+        raise ValueError("Cannot convert {} to numpy array"
+                         .format(type(tensor)))
+    return tensor
+
+
+def to_torch(ndarray):
+    if type(ndarray).__module__ == 'numpy':
+        return torch.from_numpy(ndarray)
+    elif not torch.is_tensor(ndarray):
+        raise ValueError("Cannot convert {} to torch tensor"
+                         .format(type(ndarray)))
+    return ndarray
+
+
+def im_to_numpy(img):
+    img = to_numpy(img)
+    img = np.transpose(img, (1, 2, 0))  # H*W*C
+    return img
+
+
+def im_to_torch(img):
+    img = np.transpose(img, (2, 0, 1))  # C*H*W
+    img = to_torch(img).float()
+    return img
+
+
+def torch_to_img(img):
+    img = to_numpy(torch.squeeze(img, 0))
+    img = np.transpose(img, (1, 2, 0))  # H*W*C
+    return img
+
+
+def get_subwindow_tracking(im, pos, model_sz, original_sz, avg_chans, out_mode='torch', new=False):
+
+    if isinstance(pos, float):
+        pos = [pos, pos]
+    sz = original_sz
+    im_sz = im.shape
+    c = (original_sz+1) / 2
+    context_xmin = round(pos[0] - c)  # floor(pos(2) - sz(2) / 2);
+    context_xmax = context_xmin + sz - 1
+    context_ymin = round(pos[1] - c)  # floor(pos(1) - sz(1) / 2);
+    context_ymax = context_ymin + sz - 1
+    left_pad = int(max(0., -context_xmin))
+    top_pad = int(max(0., -context_ymin))
+    right_pad = int(max(0., context_xmax - im_sz[1] + 1))
+    bottom_pad = int(max(0., context_ymax - im_sz[0] + 1))
+
+    context_xmin = context_xmin + left_pad
+    context_xmax = context_xmax + left_pad
+    context_ymin = context_ymin + top_pad
+    context_ymax = context_ymax + top_pad
+
+    # zzp: a more easy speed version
+    r, c, k = im.shape
+    if any([top_pad, bottom_pad, left_pad, right_pad]):
+        te_im = np.zeros((r + top_pad + bottom_pad, c + left_pad + right_pad, k), np.uint8)  # 0 is better than 1 initialization
+        te_im[top_pad:top_pad + r, left_pad:left_pad + c, :] = im
+        if top_pad:
+            te_im[0:top_pad, left_pad:left_pad + c, :] = avg_chans
+        if bottom_pad:
+            te_im[r + top_pad:, left_pad:left_pad + c, :] = avg_chans
+        if left_pad:
+            te_im[:, 0:left_pad, :] = avg_chans
+        if right_pad:
+            te_im[:, c + left_pad:, :] = avg_chans
+        im_patch_original = te_im[int(context_ymin):int(context_ymax + 1), int(context_xmin):int(context_xmax + 1), :]
+    else:
+        im_patch_original = im[int(context_ymin):int(context_ymax + 1), int(context_xmin):int(context_xmax + 1), :]
+
+    if not np.array_equal(model_sz, original_sz):
+        im_patch = cv2.resize(im_patch_original, (model_sz, model_sz))  # zzp: use cv to get a better speed
+    else:
+        im_patch = im_patch_original
+
+    return im_to_torch(im_patch) if out_mode in 'torch' else im_patch
+
+
+def cxy_wh_2_rect(pos, sz):
+    return np.array([pos[0]-sz[0]/2, pos[1]-sz[1]/2, sz[0], sz[1]])  # 0-index
+
+
+def rect_2_cxy_wh(rect):
+    return np.array([rect[0]+rect[2]/2, rect[1]+rect[3]/2]), np.array([rect[2], rect[3]])  # 0-index
+
+
+def get_axis_aligned_bbox(region):
+    try:
+        region = np.array([region[0][0][0], region[0][0][1], region[0][1][0], region[0][1][1],
+                           region[0][2][0], region[0][2][1], region[0][3][0], region[0][3][1]])
+    except:
+        region = np.array(region)
+    cx = np.mean(region[0::2])
+    cy = np.mean(region[1::2])
+    x1 = min(region[0::2])
+    x2 = max(region[0::2])
+    y1 = min(region[1::2])
+    y2 = max(region[1::2])
+    A1 = np.linalg.norm(region[0:2] - region[2:4]) * np.linalg.norm(region[2:4] - region[4:6])
+    A2 = (x2 - x1) * (y2 - y1)
+    s = np.sqrt(A1 / A2)
+    w = s * (x2 - x1) + 1
+    h = s * (y2 - y1) + 1
+    return cx, cy, w, h
