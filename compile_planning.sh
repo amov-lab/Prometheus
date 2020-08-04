dcaf72519910cf667f9774145f4d67ba7f605c98
Yuhua <fatmoonqyp@126.com>
2020-08-04 22:51:55 +0800

    update: move the msgs folder into common

--- compile_planning.sh
+++ compile_planning.sh
@@ -1,4 +1,4 @@
-catkin_make --source Modules/msgs --build build/msgs
+catkin_make --source Modules/common/msgs --build build/msgs
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/planning --build build/planning
 
