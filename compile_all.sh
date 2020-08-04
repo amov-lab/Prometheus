af0ace0f920d4c30f49be5c0e7cc1ab9fa28357c
Yuhua <fatmoonqyp@126.com>
2020-08-04 22:51:55 +0800

    update: move the msgs folder into common

--- compile_all.sh
+++ compile_all.sh
@@ -1,4 +1,4 @@
-catkin_make --source Modules/msgs --build build/msgs
+catkin_make --source Modules/common/msgs --build build/msgs
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/object_detection --build build/object_detection
 catkin_make --source Modules/mission --build build/mission
