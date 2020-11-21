de856cacccc6bfe51e34eb53c2d20273238d3825
jario <jariof@foxmail.com>
2020-11-21 12:10:33 +0800

    detection: add detection submodule examples

--- compile_detection.sh
+++ compile_detection.sh
@@ -1,2 +1,13 @@
+# compile common message
 catkin_make --source Modules/common/msgs --build build/msgs
+# compile object_detection
 catkin_make --source Modules/object_detection --build build/object_detection
+
+if [ ! -d "Modules/object_detection_landing" ]; then
+  # submodule object_detection_landing not exist, skip it
+  echo -e "\e[32m[INFO] SUBMODULE\e[0m \e[33mobject_detection_landing\e[0m \e[32mNOT EXIST, Skip it!\e[0m"
+else
+  echo -e "\e[32m[INFO] COMPILE \e[33mobject_detection_landing\e[0m \e[32m...\e[0m"
+  # compile object_detection_landing
+  catkin_make --source Modules/object_detection_landing --build build/object_detection_landing
+fi
