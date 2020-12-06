e85a5d30b8033db63863cb9a89c1601f915d1016
Yuhua <fatmoonqyp@126.com>
2020-12-06 13:22:37 +0800

    simulator : laserscan

--- Simulator/gazebo_simulator/py_nodes/laser_to_pointcloud.py
+++ Simulator/gazebo_simulator/py_nodes/laser_to_pointcloud.py
@@ -2,7 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import PointCloud2 as pc2
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan 
 from laser_geometry import LaserProjection
 
 class Laser2PC():
@@ -12,6 +12,26 @@ class Laser2PC():
         self.laserSub = rospy.Subscriber("/prometheus/sensors/2Dlidar_scan", LaserScan, self.laserCallback) 
 
     def laserCallback(self,data):
+        
+        # cc = LaserScan()
+
+        # cc.header = data.header
+        # cc.angle_max = data.angle_max
+        # cc.angle_min = data.angle_min
+        # cc.angle_increment = data.angle_increment
+        # cc.time_increment = data.time_increment
+        # cc.scan_time = data.scan_time
+        # cc.range_max = data.range_max
+        # cc.range_min = data.range_min
+        # cc.ranges = data.ranges
+        # cc.intensities = data.intensities
+
+        # a = len(data.ranges)
+
+        # for i in range(0,a):
+        #     if data.ranges[i] != float("inf"):
+        #         if data.ranges[i] < 0.4:
+        #             cc.ranges[i] = 0.41
 
         cloud_out = self.laserProj.projectLaser(data)
 
