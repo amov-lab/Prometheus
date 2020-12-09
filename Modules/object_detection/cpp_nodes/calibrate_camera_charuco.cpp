7b68cf8396c07d2bbaa7bcd9f6c988b3df8f4894
jario <jariof@foxmail.com>
2020-12-10 00:43:58 +0800

    detection: mipi camera support for aruco calib

--- Modules/object_detection/cpp_nodes/calibrate_camera_charuco.cpp
+++ Modules/object_detection/cpp_nodes/calibrate_camera_charuco.cpp
@@ -113,6 +113,14 @@ static bool saveCameraParams(const string &filename, Size imageSize, float aspec
 }
 
 
+std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+           std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
+           "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
+           std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
+
+
 
 /**
  */
@@ -153,6 +161,22 @@ int main(int argc, char *argv[]) {
     }
 
     bool refindStrategy = parser.get<bool>("rs");
+
+
+    int capture_width = 1280 ;
+    int capture_height = 720 ;
+    int display_width = 1280 ;
+    int display_height = 720 ;
+    int framerate = 30 ;
+    int flip_method = 0 ;
+
+    std::string pipeline = gstreamer_pipeline(capture_width,
+	capture_height,
+	display_width,
+	display_height,
+	framerate,
+	flip_method);
+
     int camId = parser.get<int>("ci");
     String video;
 
@@ -171,7 +195,10 @@ int main(int argc, char *argv[]) {
         inputVideo.open(video);
         waitTime = 0;
     } else {
-        inputVideo.open(camId);
+        if (camId == 99)
+            inputVideo.open(pipeline, cv::CAP_GSTREAMER);
+        else
+            inputVideo.open(camId);
         waitTime = 10;
     }
 
