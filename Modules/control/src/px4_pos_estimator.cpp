7d04adf9efd87412d9f49f4b567b81bcb815c47d
Yuhua <fatmoonqyp@126.com>
2020-12-24 10:40:27 +0800

    mission:autonomous_landing

--- Modules/control/src/px4_pos_estimator.cpp
+++ Modules/control/src/px4_pos_estimator.cpp
@@ -109,6 +109,10 @@ void mocap_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
     {
         // Read the Drone Position from the Vrpn Package [Frame: Vicon]  (Vicon to ENU frame)
         pos_drone_mocap = Eigen::Vector3d(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
+
+        pos_drone_mocap[0] = pos_drone_mocap[0] - pos_offset[0];
+        pos_drone_mocap[1] = pos_drone_mocap[1] - pos_offset[1];
+        pos_drone_mocap[2] = pos_drone_mocap[2] - pos_offset[2];
         // Read the Quaternion from the Vrpn Package [Frame: Vicon[ENU]]
         q_mocap = Eigen::Quaterniond(msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z);
     }
@@ -118,6 +122,9 @@ void mocap_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
         pos_drone_mocap = Eigen::Vector3d(-msg->pose.position.x, msg->pose.position.z, msg->pose.position.y);
         // Read the Quaternion from the Vrpn Package [Frame: Vicon[ENU]]
         q_mocap = Eigen::Quaterniond(msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.z, msg->pose.orientation.y); //Y-up convention, switch the q2 & q3
+        pos_drone_mocap[0] = pos_drone_mocap[0] - pos_offset[0];
+        pos_drone_mocap[1] = pos_drone_mocap[1] - pos_offset[1];
+        pos_drone_mocap[2] = pos_drone_mocap[2] - pos_offset[2];
     }
 
     // Transform the Quaternion to Euler Angles
@@ -131,9 +138,6 @@ void gazebo_cb(const nav_msgs::Odometry::ConstPtr &msg)
     if (msg->header.frame_id == "world")
     {
         pos_drone_gazebo = Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
-        // pos_drone_gazebo[0] = msg->pose.pose.position.x + pos_offset[0];
-        // pos_drone_gazebo[1] = msg->pose.pose.position.y + pos_offset[1];
-        // pos_drone_gazebo[2] = msg->pose.pose.position.z + pos_offset[2];
 
         q_gazebo = Eigen::Quaterniond(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
         Euler_gazebo = quaternion_to_euler(q_gazebo);
