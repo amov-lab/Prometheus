536714db39febab511e08ebce630b5d3e183c3c5
Yuhua <fatmoonqyp@126.com>
2020-12-25 10:36:19 +0800

    planning : rewrite the astar planning

--- Modules/planning/global_planning/src/occupy_map.cpp
+++ Modules/planning/global_planning/src/occupy_map.cpp
@@ -1,31 +1,36 @@
 #include <occupy_map.h>
 
-namespace global_planner{
-
-void Occupy_map::setparam(ros::NodeHandle& nh)
+namespace Global_Planning
 {
-    // 地图分辨率，单位：米
-    nh.param("astar/resolution_astar", resolution_,  0.2);
-    this->inv_resolution_ = 1.0 / resolution_;
-    // 地图膨胀距离，单位：米
-    nh.param("astar/inflate", inflate_,  0.3);
-    // 地图实际尺寸，单位：米
-    nh.param("map/map_size_x", map_size_3d_(0), 10.0);
-    nh.param("map/map_size_y", map_size_3d_(1), 10.0);
-    nh.param("map/map_size_z", map_size_3d_(2), 5.0);
+// 初始化函数
+void Occupy_map::init(ros::NodeHandle& nh)
+{
+    // TRUE代表2D平面规划及搜索,FALSE代表3D 
+    nh.param("global_planner/is_2D", is_2D, true); 
+    // 2D规划时,定高高度
+    nh.param("global_planner/fly_height_2D", fly_height_2D, 1.0);
     // 地图原点
     nh.param("map/origin_x", origin_(0), -5.0);
     nh.param("map/origin_y", origin_(1), -5.0);
     nh.param("map/origin_z", origin_(2), 0.0);
-    // 天花板、地板高度
-    nh.param("map/ceil_height_", ceil_height_, 4.9);
-    nh.param("map/floor_height_", floor_height_, 0.1);
+    // 地图实际尺寸，单位：米
+    nh.param("map/map_size_x", map_size_3d_(0), 10.0);
+    nh.param("map/map_size_y", map_size_3d_(1), 10.0);
+    nh.param("map/map_size_z", map_size_3d_(2), 5.0);
+    // 地图分辨率，单位：米
+    nh.param("map/resolution", resolution_,  0.2);
+    // 地图膨胀距离，单位：米
+    nh.param("map/inflate", inflate_,  0.3);
 
-    inflate_cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/planning/global_inflate_cloud", 1);
-}
+    // 发布 地图rviz显示
+    global_pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("/prometheus/planning/global_pcl",  10); 
+    // 发布膨胀后的点云
+    inflate_pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("/prometheus/planning/global_inflate_pcl", 1);
+ 
+    // 发布二维占据图？
+    // 发布膨胀后的二维占据图？
 
-void Occupy_map::init(void)
-{
+    this->inv_resolution_ = 1.0 / resolution_;
     for (int i = 0; i < 3; ++i)
     {
         // 占据图尺寸 = 地图尺寸 / 分辨率
@@ -38,15 +43,40 @@ void Occupy_map::init(void)
 
     min_range_ = origin_;
     max_range_ = origin_ + map_size_3d_;   
+
+    // 对于二维情况，重新限制点云高度
+    if(is_2D == true)
+    {
+        min_range_(2) = fly_height_2D - resolution_;
+        max_range_(2) = fly_height_2D + resolution_;
+    }
 }
 
-void Occupy_map::setEnvironment(const sensor_msgs::PointCloud2ConstPtr & global_point)
+// 地图更新函数 - 输入：全局点云
+void Occupy_map::map_update_gpcl(const sensor_msgs::PointCloud2ConstPtr & global_point)
 {
-    global_env_ = global_point;
     has_global_point = true;
+    global_env_ = global_point;
+}
+
+// 地图更新函数 - 输入：局部点云
+void Occupy_map::map_update_lpcl(const sensor_msgs::PointCloud2ConstPtr & local_point, const nav_msgs::Odometry & odom)
+{
+    has_global_point = true;
+// 待江涛更新
+// 将传递过来的局部点云转为全局点云
+}
+
+// 地图更新函数 - 输入：laser
+void Occupy_map::map_update_laser(const sensor_msgs::LaserScanConstPtr & local_point, const nav_msgs::Odometry & odom)
+{
+    has_global_point = true;
+// 待更新
+// 将传递过来的数据转为全局点云
 }
 
 // 当global_planning节点接收到点云消息更新时，进行设置点云指针并膨胀
+// Astar规划路径时，采用的是此处膨胀后的点云（setOccupancy只在本函数中使用）
 void Occupy_map::inflate_point_cloud(void)
 {
     if(!has_global_point)
@@ -55,65 +85,43 @@ void Occupy_map::inflate_point_cloud(void)
         return;
     }
 
+    // 发布未膨胀点云
+    global_pcl_pub.publish(*global_env_);
+
     //记录开始时间
     ros::Time time_start = ros::Time::now();
 
-    // 更新的全局点云
+    // 转化为PCL的格式进行处理
     pcl::PointCloud<pcl::PointXYZ> latest_global_cloud_;
     pcl::fromROSMsg(*global_env_, latest_global_cloud_);
 
-    if ((int)latest_global_cloud_.points.size() == 0)  return;
+    //printf("time 1 take %f [s].\n",   (ros::Time::now()-time_start).toSec());
+
+    if ((int)latest_global_cloud_.points.size() == 0)  
+    {return;}
 
     pcl::PointCloud<pcl::PointXYZ> cloud_inflate_vis_;
     cloud_inflate_vis_.clear();
-
-    //  printf("point size: %d, ifn_num: %d\n", latest_global_cloud_.points.size(), ifn);
-
-    // 创建tf的监听器
-    tf::TransformListener listener;
-    try{
-        // 等待获取监听信息base_link和base_laser
-        listener.waitForTransform("world", "map", ros::Time(0), ros::Duration(3.0));
-    }
-    catch(tf::TransformException& ex){
-        ROS_ERROR("Received an exception trying to transform a point from \"base_laser\" to \"base_link\": %s", ex.what());
-    }
-
-    // iterate the map
-    pcl::PointCloud<pcl::PointXYZ> world_pcl;
-    geometry_msgs::PointStamped world_map_point;
 
     // 膨胀格子数 = 膨胀距离/分辨率
     // ceil返回大于或者等于指定表达式的最小整数
     const int ifn = ceil(inflate_ * inv_resolution_);
 
-    pcl::PointXYZ pt, pt_inf;
+    pcl::PointXYZ pt_inf;
     Eigen::Vector3d p3d, p3d_inf;
 
     // 遍历全局点云中的所有点
     for (size_t i = 0; i < latest_global_cloud_.points.size(); ++i) 
     {
         // 取出第i个点
-        pt = latest_global_cloud_.points[i];
-        p3d(0) = pt.x;
-        p3d(1) = pt.y;
-        p3d(2) = pt.z;
+        p3d(0) = latest_global_cloud_.points[i].x;
+        p3d(1) = latest_global_cloud_.points[i].y;
+        p3d(2) = latest_global_cloud_.points[i].z;
 
-        // 如果点云并非world系，则进行左边转换，并存入pt
-        // 会有这种情况吗？
-        if(global_env_->header.frame_id != "world")
+        // 若取出的点不在地图内（膨胀时只考虑地图范围内的点）
+        if(!isInMap(p3d))
         {
-            geometry_msgs::PointStamped map_point;
-            map_point.header.frame_id = "map";
-            map_point.point.x = pt.x; 
-            map_point.point.y = pt.y; 
-            map_point.point.z = pt.z; 
-
-            listener.transformPoint("world", map_point, world_map_point);
-            printf("** map_point 3d: [%f,  %f,  %f],   world_map_point: [%f, %f, %f]\n", map_point.point.x, map_point.point.y, map_point.point.z, 
-            world_map_point.point.x, world_map_point.point.y, world_map_point.point.z);
-
-            pt = pcl::PointXYZ(world_map_point.point.x, world_map_point.point.y, world_map_point.point.z);
+            continue;
         }
 
         // 根据膨胀距离，膨胀该点
@@ -121,13 +129,16 @@ void Occupy_map::inflate_point_cloud(void)
             for (int y = -ifn; y <= ifn; ++y)
                 for (int z = -ifn; z <= ifn; ++z) 
                 {
-                    // 为什么Z轴膨胀一半呢？
-                    p3d_inf(0) = pt_inf.x = pt.x + x * resolution_;
-                    p3d_inf(1) = pt_inf.y = pt.y + y * resolution_;
-                    p3d_inf(2) = pt_inf.z = pt.z + 0.5 * z * resolution_;
+                    // 为什么Z轴膨胀一半呢？ z 轴其实可以不膨胀
+                    p3d_inf(0) = pt_inf.x = p3d(0) + x * resolution_;
+                    p3d_inf(1) = pt_inf.y = p3d(1) + y * resolution_;
+                    p3d_inf(2) = pt_inf.z = p3d(2) + 0.5 * z * resolution_;
 
-                    // 若膨胀的点超过天花板或者低于地面，则退出
-                    if(p3d_inf(2)>ceil_height_ || p3d_inf(2)<0) break;
+                    // 若膨胀的点不在地图内（膨胀时只考虑地图范围内的点）
+                    if(!isInMap(p3d_inf))
+                    {
+                        continue;
+                    }
 
                     cloud_inflate_vis_.push_back(pt_inf);
 
@@ -136,40 +147,26 @@ void Occupy_map::inflate_point_cloud(void)
                 }
     }
 
-    // cloud_inflate_vis_.width = cloud_inflate_vis_.points.size();
-    // cloud_inflate_vis_.height = 1;
-    // cloud_inflate_vis_.is_dense = true;
     cloud_inflate_vis_.header.frame_id = "world";
 
-    /* ---------- add ceil  and floor---------- */
-    if (ceil_height_ > 0.0) 
-    {
-        for (double cx = min_range_(0); cx <= max_range_(1); cx += resolution_)
-            for (double cy =min_range_(0); cy <= max_range_(1); cy += resolution_) 
-            {
-                this->setOccupancy(Eigen::Vector3d(cx, cy, ceil_height_), 1);
-                cloud_inflate_vis_.push_back(pcl::PointXYZ(cx, cy, ceil_height_));
-            }
-    }
-
-    if (floor_height_ > 0.0) {
-        for (double cx = min_range_(0); cx <= max_range_(1); cx += resolution_)
-            for (double cy =min_range_(0); cy <= max_range_(1); cy += resolution_) 
-            {
-                this->setOccupancy(Eigen::Vector3d(cx, cy, floor_height_), 1);
-                cloud_inflate_vis_.push_back(pcl::PointXYZ(cx, cy, floor_height_));
-            }
-    }
-
+    // 转化为ros msg发布
     sensor_msgs::PointCloud2 map_inflate_vis;
     pcl::toROSMsg(cloud_inflate_vis_, map_inflate_vis);
 
-    inflate_cloud_pub_.publish(map_inflate_vis);
+    inflate_pcl_pub.publish(map_inflate_vis);
 
-    // 一般需要0.3秒左右（有点久，Astar更新频率是多久呢？ 怎么才能提高膨胀效率呢？）
-    printf("inflate global point take %f.\n",   (ros::Time::now()-time_start).toSec());
+    static int exec_num=0;
+    exec_num++;
+
+    // 此处改为根据循环时间计算的数值
+    if(exec_num == 20)
+    {
+        // 膨胀地图效率与地图大小有关（有点久，Astar更新频率是多久呢？ 怎么才能提高膨胀效率呢？）
+        printf("inflate global point take %f [s].\n",   (ros::Time::now()-time_start).toSec());
+        exec_num=0;
+    }  
+
 }
-
 
 void Occupy_map::setOccupancy(Eigen::Vector3d pos, int occ) 
 {
@@ -187,7 +184,7 @@ void Occupy_map::setOccupancy(Eigen::Vector3d pos, int occ)
     Eigen::Vector3i id;
     posToIndex(pos, id);
 
-    // 设置为占据/不占据 索引是如何索引的？
+    // 设置为占据/不占据 索引是如何索引的？ [三维地图 变 二维数组]
     // 假设10*10*10米，分辨率1米，buffer大小为 1000 （即每一个占格都对应一个buffer索引）
     // [0.1,0.1,0.1] 对应索引为[0,0,0]， buffer索引为 0  
     // [9.9,9.9,9.9] 对应索引为[9,9,9]， buffer索引为 900+90+9 = 999
@@ -198,6 +195,7 @@ bool Occupy_map::isInMap(Eigen::Vector3d pos)
 {
     // min_range就是原点，max_range就是原点+地图尺寸
     // 比如设置0,0,0为原点，[0,0,0]点会被判断为不在地图里
+    //　同时　对于２Ｄ情况，超出飞行高度的数据也会认为不在地图内部
     if (pos(0) < min_range_(0) + 1e-4 || pos(1) < min_range_(1) + 1e-4 || pos(2) < min_range_(2) + 1e-4) 
     {
         return false;
@@ -211,7 +209,7 @@ bool Occupy_map::isInMap(Eigen::Vector3d pos)
     return true;
 }
 
-bool Occupy_map::check_safety(Eigen::Vector3d& pos, double check_distance/*, Eigen::Vector3d& map_point*/)
+bool Occupy_map::check_safety(Eigen::Vector3d& pos, double check_distance)
 {
     if(!isInMap(pos))
     {
@@ -293,5 +291,4 @@ int Occupy_map::getOccupancy(Eigen::Vector3i id)
         
     return occupancy_buffer_[id(0) * grid_size_(1) * grid_size_(2) + id(1) * grid_size_(2) + id(2)];
 }
-
 }
