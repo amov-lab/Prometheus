636eb8afcf7e6d26cd790eb8774657bf89b8c066
Yuhua <fatmoonqyp@126.com>
2020-12-25 17:23:17 +0800

    planning : update local planning

--- Modules/planning/local_planner/include/vfh.h
+++ Modules/planning/local_planner/include/vfh.h
@@ -1,14 +1,12 @@
 #ifndef VFH_H
 #define VFH_H
 
-
+#include <ros/ros.h>
 #include <Eigen/Eigen>
 #include <iostream>
 #include <algorithm>
 #include <iostream>
 
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Empty.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -16,7 +14,6 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-// #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl_conversions/pcl_conversions.h>
 
 #include "tools.h"
@@ -24,57 +21,50 @@
 
 using namespace std;
 
-namespace local_planner{
+namespace Local_Planning
+{
 
 extern ros::Publisher message_pub;
 
-class VFH: public local_planning_alg{
+class VFH: public local_planning_alg
+{
 private:
+    //　参数
+    double inflate_distance;
+    double sensor_max_range;
+    double safe_distance;
+
     bool has_local_map_;
+    bool has_odom_;
+    bool is_2D;
     
     double goalWeight, obstacle_weight;
 
-    double sensing_distance;   // 感知距离
-
-    double ground_height;
-    double ground_safe_height;
     double inflate_and_safe_distance;
-    double inflate_distance;
 
-    double safe_distance;
     double limit_v_norm;
 
     double* Hdata;
     double  Hres;
     int Hcnt;  // 直方图个数
-    // Eigen::Matrix<double, 3, 1> push_force;
-    // Eigen::Matrix<double, 3, 1> attractive_force;
 
     pcl::PointCloud<pcl::PointXYZ> latest_local_pcl_;
     sensor_msgs::PointCloud2ConstPtr  local_map_ptr_;
-
-    ros::Time begin_update_map;
     nav_msgs::Odometry cur_odom_;
-    Eigen::Matrix<double, 3, 1>  cur_goal; 
 
     bool isIgnored(float x, float y, float z, float ws);
     int find_Hcnt(double angle);
     double find_angle(int cnt);
     double angle_error(double angle1, double angle2);
-
     void generate_voxel_data(double angle_cen, double angle_range, double val);
-
     int find_optimization_path(void);
+
 public:
-    int replan{0};
-    
+
     virtual void set_odom(nav_msgs::Odometry cur_odom);
-    
     virtual void set_local_map(sensor_msgs::PointCloud2ConstPtr &local_map_ptr);
     virtual void set_local_map_pcl(pcl::PointCloud<pcl::PointXYZ>::Ptr &pcl_ptr);
-    
-    virtual int compute_force(Eigen::Matrix<double, 3, 1> &goal, Eigen::Matrix<double, 3, 1> current_odom, Eigen::Vector3d &desired_vel);
-
+    virtual int compute_force(Eigen::Vector3d  &goal, Eigen::Vector3d &desired_vel);
     virtual void init(ros::NodeHandle& nh);
 
     VFH(){}
--- Modules/planning/local_planner/src/tools.cpp
+++ Modules/planning/local_planner/src/tools.cpp
@@ -1,14 +1,5 @@
 #include "tools.h"
-namespace local_planner
+namespace Local_Planning
 {
-
-// void pub_msg(ros::Publisher & puber, std::string mmm, int type){
-//     prometheus_msgs::Message exect_msg;
-//     exect_msg.header.stamp = ros::Time::now();
-//     exect_msg.message_type=type;
-//     exect_msg.content = mmm;
-//     puber.publish(exect_msg);
-// }
-
 ros::Publisher message_pub;
 }
