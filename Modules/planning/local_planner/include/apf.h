c2a78aa992e6774d9b432e6c5db76cc83e2d07da
Yuhua <fatmoonqyp@126.com>
2020-12-25 17:23:17 +0800

    planning : update local planning

--- Modules/planning/local_planner/include/apf.h
+++ Modules/planning/local_planner/include/apf.h
@@ -1,14 +1,12 @@
 #ifndef APF_H
 #define APF_H
 
-
+#include <ros/ros.h>
 #include <Eigen/Eigen>
 #include <iostream>
 #include <algorithm>
 #include <iostream>
 
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Empty.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -16,47 +14,48 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-// #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl_conversions/pcl_conversions.h>
 
 #include "tools.h"
 #include "local_planning_alg.h"
 using namespace std;
 
-namespace local_planner{
+namespace Local_Planning
+{
 
 extern ros::Publisher message_pub;
 
-class APF:public local_planning_alg{
+class APF:public local_planning_alg
+{
 private:
-    bool has_local_map_;
-    
-    double obs_distance;
-    double min_dist;
-    double k_push;
-
+    //　参数
+    double inflate_distance;
+    double sensor_max_range;
     double max_att_dist;
+    double k_push;
     double k_att;
+    double min_dist;
     double ground_height;
     double ground_safe_height;
-    double inflate_distance;
     double safe_distance;
 
-    Eigen::Matrix<double, 3, 1> push_force;
-    Eigen::Matrix<double, 3, 1> attractive_force;
+    bool has_local_map_;
+    bool has_odom_;
+    bool is_2D;
+    
+    Eigen::Vector3d push_force;
+    Eigen::Vector3d attractive_force;
 
     pcl::PointCloud<pcl::PointXYZ> latest_local_pcl_;
     sensor_msgs::PointCloud2ConstPtr  local_map_ptr_;
-
-    ros::Time begin_update_map;
     nav_msgs::Odometry cur_odom_;
     
 public:
-    int replan{0};
+
     virtual void set_odom(nav_msgs::Odometry cur_odom);
     virtual void set_local_map(sensor_msgs::PointCloud2ConstPtr &local_map_ptr);
     virtual void set_local_map_pcl(pcl::PointCloud<pcl::PointXYZ>::Ptr &pcl_ptr);
-    virtual int compute_force(Eigen::Matrix<double, 3, 1> &goal, Eigen::Matrix<double, 3, 1> current_odom, Eigen::Vector3d &desired_vel);
+    virtual int compute_force(Eigen::Vector3d &goal, Eigen::Vector3d &desired_vel);
     virtual void init(ros::NodeHandle& nh);
     APF(){}
     ~APF(){}
--- Modules/planning/local_planner/src/apf.cpp
+++ Modules/planning/local_planner/src/apf.cpp
@@ -1,15 +1,33 @@
 #include "apf.h"
 
-namespace local_planner
+namespace Local_Planning
 {
 
-void APF::set_local_map(sensor_msgs::PointCloud2ConstPtr &local_map_ptr){
+void APF::init(ros::NodeHandle& nh)
+{
+    has_local_map_ = false;
+
+    nh.param("apf/inflate_distance", inflate_distance, 0.20);  // 感知障碍物距离
+    nh.param("apf/sensor_max_range", sensor_max_range, 2.5);  // 感知障碍物距离
+    nh.param("apf/k_push", k_push, 0.8);                         // 推力增益
+    nh.param("apf/k_att", k_att, 0.4);                                  // 引力增益
+    nh.param("apf/min_dist", min_dist, 0.2);                            // 最小壁障距离
+    nh.param("apf/max_att_dist", max_att_dist, 5.0);             // 最大吸引距离
+    nh.param("apf/ground_height", ground_height, 0.1);  // 地面高度
+    nh.param("apf/ground_safe_height", ground_safe_height, 0.2);  // 地面安全距离
+    nh.param("apf/safe_distance", safe_distance, 0.15); // 安全停止距离
+
+    // TRUE代表2D平面规划及搜索,FALSE代表3D 
+    nh.param("local_planner/is_2D", is_2D, true); 
+}
+
+void APF::set_local_map(sensor_msgs::PointCloud2ConstPtr &local_map_ptr)
+{
     local_map_ptr_ = local_map_ptr;
     ros::Time begin_load_point_cloud = ros::Time::now();
 
     pcl::fromROSMsg(*local_map_ptr, latest_local_pcl_);
 
-    begin_update_map = ros::Time::now();
     has_local_map_=true;
 }
 
@@ -19,146 +37,145 @@ void APF::set_local_map_pcl(pcl::PointCloud<pcl::PointXYZ>::Ptr &pcl_ptr)
     has_local_map_=true;
 }
 
-void APF::set_odom(nav_msgs::Odometry cur_odom){
+void APF::set_odom(nav_msgs::Odometry cur_odom)
+{
     cur_odom_ = cur_odom;
+    has_odom_=true;
 }
 
-
-int APF::compute_force(Eigen::Matrix<double, 3, 1> &goal, Eigen::Matrix<double, 3, 1> current_odom, Eigen::Vector3d &desired_vel){
-    int local_planner_state=0;  // 0 for not init; 1for safe; 2 for dangerous
+int APF::compute_force(Eigen::Vector3d &goal, Eigen::Vector3d &desired_vel)
+{
+    // 0 for not init; 1for safe; 2 for dangerous
+    int local_planner_state=0;  
     int safe_cnt=0;
 
-    if(!has_local_map_)
+    if(!has_local_map_|| !has_odom_)
         return 0;
 
     if ((int)latest_local_pcl_.points.size() == 0) 
         return 0;
 
-    if (isnan(current_odom(0)) || isnan(current_odom(1)) || isnan(current_odom(2)))
-        return 0;
-
     if (isnan(goal(0)) || isnan(goal(1)) || isnan(goal(2)))
         return 0;
 
-    vector<Eigen::Vector3d> obstacles;
-    pcl::PointXYZ pt;
-    Eigen::Vector3d p3d;
+    //　当前位置
+    Eigen::Vector3d current_pos;
+    current_pos[0] = cur_odom_.pose.pose.position.x;
+    current_pos[1] = cur_odom_.pose.pose.position.y;
+    current_pos[2] = cur_odom_.pose.pose.position.z;
+
     ros::Time begin_collision = ros::Time::now();
-    // ROS_INFO("point size: %d", latest_local_pcl_.points.size());
 
     // 引力
-    Eigen::Vector3d odom2goal = goal - current_odom;
+    Eigen::Vector3d uav2goal = goal - current_pos;
     // 不考虑高度影响
-    odom2goal(2) = 0.0;
-    double dist_att = odom2goal.norm();
+    uav2goal(2) = 0.0;
+    double dist_att = uav2goal.norm();
     if(dist_att > max_att_dist)
     {
-        odom2goal = max_att_dist * odom2goal/dist_att ;
-        attractive_force = k_att * odom2goal;
-    }else
-    {
-        attractive_force = k_att * odom2goal;
+        uav2goal = max_att_dist * uav2goal/dist_att ;
     }
+    //　计算吸引力
+    attractive_force = k_att * uav2goal;
 
     // 排斥力
     double uav_height = cur_odom_.pose.pose.position.z;
     push_force = Eigen::Vector3d(0.0, 0.0, 0.0);
-    for (size_t i = 0; i < latest_local_pcl_.points.size(); ++i) {
-        pt = latest_local_pcl_.points[i];
-        p3d(0) = pt.x, p3d(1) = pt.y, p3d(2) = pt.z;
-        // 不考虑高度的影响
-        p3d(2) = 0.0;
-        
 
-        Eigen::Matrix<double, 3,1> current_odom_local(0.0, 0,0);
-        // Eigen::Matrix<double, 3,1> current_odom_local = current_odom;
-        // remove the ground point 
+    Eigen::Vector3d p3d;
+    vector<Eigen::Vector3d> obstacles;
+    
+    //　根据局部点云计算排斥力（是否可以考虑对点云进行降采样？）
+    for (size_t i = 0; i < latest_local_pcl_.points.size(); ++i) 
+    {
+        p3d(0) = latest_local_pcl_.points[i].x;
+        p3d(1) = latest_local_pcl_.points[i].y;
+        p3d(2) = latest_local_pcl_.points[i].z;
+
+        Eigen::Vector3d current_pos_local(0.0, 0.0, 0.0);
+        
+        //　低于地面高度，则不考虑该点的排斥力
         double point_height_global = uav_height+p3d(2);
         if(fabs(point_height_global)<ground_height)
             continue;
 
-        double dist_push = (current_odom_local - p3d).norm();
-        if(dist_push > obs_distance || isnan(dist_push))
+        //　超出最大感知距离，则不考虑该点的排斥力
+        double dist_push = (current_pos_local - p3d).norm();
+        if(dist_push > sensor_max_range || isnan(dist_push))
             continue;
 
+        //　考虑膨胀距离
         dist_push = dist_push - inflate_distance;
+
         // 如果当前的观测点中，包含小于安全停止距离的点，进行计数
-        if(dist_push < safe_distance){
+        if(dist_push < safe_distance)
+        {
             safe_cnt++;
         }
         
-        if(dist_push < min_dist){
-            // should be addressed carefully.
-            // printf("the distance is very dangerous, dist: %f\n", dist_push);
+        //　小于最小距离时，则增大该距离，从而增大排斥力
+        if(dist_push < min_dist)
+        {
             dist_push = min_dist /1.5;
         }
 
         obstacles.push_back(p3d);
-        double push_gain = k_push * (1/dist_push - 1/obs_distance)* 1.0/(dist_push * dist_push);
+        double push_gain = k_push * (1/dist_push - 1/sensor_max_range)* 1.0/(dist_push * dist_push);
 
-        if(dist_att<1.0){
+        if(dist_att<1.0)
+        {
             push_gain *= dist_att;  // to gaurantee to reach the goal.
         }
 
-        push_force += push_gain * (current_odom_local - p3d)/dist_push;
+        push_force += push_gain * (current_pos_local - p3d)/dist_push;
     }
 
-    if(obstacles.size() != 0){
+    //　平均排斥力
+    if(obstacles.size() != 0)
+    {
         push_force=push_force/obstacles.size();
     }
 
     Eigen::Quaterniond cur_rotation_local_to_global(cur_odom_.pose.pose.orientation.w, 
-                                                                                                            cur_odom_.pose.pose.orientation.x,  
-                                                                                                            cur_odom_.pose.pose.orientation.y,  
-                                                                                                            cur_odom_.pose.pose.orientation.z); 
+                                                    cur_odom_.pose.pose.orientation.x,  
+                                                    cur_odom_.pose.pose.orientation.y,  
+                                                    cur_odom_.pose.pose.orientation.z); 
 
-    // printf("odom q:[%f, %f, %f, %f]\n", cur_odom_.pose.pose.orientation.w, 
-    //                                                                                                         cur_odom_.pose.pose.orientation.x,  
-    //                                                                                                         cur_odom_.pose.pose.orientation.y,  
-    //                                                                                                         cur_odom_.pose.pose.orientation.z);
 
     Eigen::Matrix<double,3,3> rotation_mat_local_to_global = cur_rotation_local_to_global.toRotationMatrix();
 
-
     push_force = rotation_mat_local_to_global * push_force; 
 
-    if(uav_height<ground_safe_height){
-            // printf("[compute_force]: near the ground, the height%f \n", uav_height);
-            push_force = push_force + Eigen::Matrix<double, 3, 1>(0, 0, (ground_safe_height-uav_height)*3.0);
-    }
-
-
-    // ROS_INFO("push force: [%f, %f, %f], attractive force: [%f, %f, %f], obs size: %d, obs_dis: %f, k_push: %f", push_force(0), push_force(1), 
-    // push_force(2), attractive_force(0), attractive_force(1), attractive_force(2), obstacles.size(), obs_distance, k_push);
-    
     // 合力
     desired_vel = push_force + attractive_force;
-    // 处理高度
-    desired_vel(2) = 0.0;
+
+    if(is_2D)
+    {
+        desired_vel[2] = 0.0;
+    }
+
     // 如果不安全的点超出，
-    if(safe_cnt>5){
+    if(safe_cnt>10)
+    {
         local_planner_state = 2;  //成功规划，但是飞机不安全
-    }else{
+    }else
+    {
         local_planner_state =1;  //成功规划， 安全
     }
+
+    static int exec_num=0;
+    exec_num++;
+
+    // 此处改为根据循环时间计算的数值
+    if(exec_num == 50)
+    {
+        printf("APF calculate take %f [s].\n",   (ros::Time::now()-begin_collision).toSec());
+        exec_num=0;
+    }  
 
     return local_planner_state;
 }
 
-void APF::init(ros::NodeHandle& nh){
-    has_local_map_ = false;
-
-    nh.param("apf/inflate_distance", inflate_distance, 0.20);  // 感知障碍物距离
-    nh.param("apf/obs_distance", obs_distance, 2.5);  // 感知障碍物距离
-    nh.param("apf/k_push", k_push, 0.8);                         // 推力增益
-    nh.param("apf/k_att", k_att, 0.4);                                  // 引力增益
-    nh.param("apf/min_dist", min_dist, 0.2);                            // 最小壁障距离
-    nh.param("apf/max_att_dist", max_att_dist, 5.0);             // 最大吸引距离
-    nh.param("apf/ground_height", ground_height, 0.1);  // 地面高度
-    nh.param("apf/ground_safe_height", ground_safe_height, 0.2);  // 地面安全距离
-    nh.param("apf/safe_distance", safe_distance, 0.15); // 安全停止距离
-
-}
 
 
 }
