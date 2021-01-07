34d468ec309f4d341fe43066d5ac8282c94775f0
qi-x1e <fatmoonqyp@126.com>
2021-01-07 22:56:49 +0800

    update compile_all.sh

--- compile_all.sh
+++ compile_all.sh
@@ -4,7 +4,7 @@ catkin_make --source Modules/object_detection --build build/object_detection
 catkin_make --source Modules/mission --build build/mission
 catkin_make --source Modules/slam --build build/slam
 catkin_make --source Modules/planning --build build/planning
-catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
 catkin_make --source Modules/ground_station --build build/ground_station
-
+catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
+catkin_make --source Experiment --build build/prometheus_experiment
 
