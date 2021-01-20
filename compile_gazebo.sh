a751d9b6af1ba192381a9f50805dd8f5c091651b
jario <jariof@foxmail.com>
2021-01-20 16:43:23 +0800

    add gazebo compile sh

---
+++ compile_gazebo.sh
@@ -0,0 +1,4 @@
+catkin_make --source Modules/common/msgs --build build/msgs
+catkin_make --source Modules/ground_station --build build/ground_station
+catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
+
