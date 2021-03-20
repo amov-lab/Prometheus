ef657c946bba9588496f2b7a8eb8e3ff9daece66
qi <fatmoonqyp@126.com>
2021-03-20 18:52:13 +0800

    add gimbal circle tracking

---
+++ Modules/mission/gimbal_control/gimbal_control_circle.cpp
@@ -0,0 +1,268 @@
+//无人机追踪目标（此处先直接用目标位置进行追踪）
+//无人机吊舱根据视觉算法的反馈调节角度，保证目标在相机中心位置
+//ROS 头文件
+#include <ros/ros.h>
+#include <iostream>
+#include "mission_utils.h"
+#include "gimbal_control.h"
+#include "message_utils.h"
+#include <prometheus_msgs/PositionReference.h>
+
+using namespace std;
+using namespace Eigen;
+
+#define NODE_NAME "gimbal_control_demo"
+#define PI 3.1415926
+
+bool hold_mode;
+Eigen::Vector3d gimbal_att_sp_deg;
+Eigen::Vector3d gimbal_att;
+Eigen::Vector3d gimbal_att_deg;
+Eigen::Vector3d gimbal_att_rate;
+Eigen::Vector3d gimbal_att_rate_deg;
+nav_msgs::Odometry GroundTruth;             // 降落板真实位置（仿真中由Gazebo插件提供）
+float sight_angle[2];
+
+float start_point[3];    // 起始降落位置
+Eigen::Vector3d roi_point;
+//---------------------------------------Drone---------------------------------------------
+prometheus_msgs::DroneState _DroneState;    // 无人机状态
+
+
+prometheus_msgs::ControlCommand Command_Now;                               //发送给控制模块 [px4_pos_controller.cpp]的命令
+
+Eigen::Vector3d mav_pos_;
+
+Eigen::Vector3f circle_center;
+float circle_radius = 2.0;
+float linear_vel = 1.0;
+float direction = 1.0;
+
+prometheus_msgs::PositionReference Circle_trajectory_generation(float time_from_start);
+void printf_result();
+
+void groundtruth_cb(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    GroundTruth = *msg;
+    circle_center[0] = GroundTruth.pose.pose.position.x;
+    circle_center[1] = GroundTruth.pose.pose.position.y;
+    circle_center[2] = GroundTruth.pose.pose.position.z + 2.0;
+    
+    roi_point[0] = GroundTruth.pose.pose.position.x;
+    roi_point[1] = GroundTruth.pose.pose.position.y;
+    roi_point[2] = GroundTruth.pose.pose.position.z;
+}
+void drone_state_cb(const prometheus_msgs::DroneState::ConstPtr& msg)
+{
+    _DroneState = *msg;
+   
+    mav_pos_ << _DroneState.position[0],_DroneState.position[1],_DroneState.position[2];
+
+    Eigen::Vector3d error_vec;
+    double distance_2d;
+
+    error_vec = roi_point - mav_pos_;
+
+    distance_2d = std::sqrt(error_vec(0) * error_vec(0) + error_vec(1) * error_vec(1));
+
+    gimbal_att_sp_deg[0] = std::atan2(error_vec(2), distance_2d)/PI*180; //pitch
+    gimbal_att_sp_deg[1] = 0.0;
+    gimbal_att_sp_deg[2] = -std::atan2(error_vec(1), error_vec(0))/PI*180;//yaw
+   
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>主函数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "gimbal_control_circle");
+    ros::NodeHandle nh("~");
+
+    // 节点运行频率： 20hz 
+    ros::Rate rate(20.0);
+
+    roi_point << 0,0,0;
+
+    // 初始起飞点
+    nh.param<float>("start_point_x", start_point[0], 0.0);
+    nh.param<float>("start_point_y", start_point[1], 1.0);
+    nh.param<float>("start_point_z", start_point[2], 1.5);
+
+    gimbal_control gimbal_control_;
+    
+    //【订阅】地面真值，此信息仅做比较使用 不强制要求提供
+    ros::Subscriber groundtruth_sub = nh.subscribe<nav_msgs::Odometry>("/ground_truth/landing_pad", 10, groundtruth_cb);
+
+    //【订阅】无人机状态
+    ros::Subscriber drone_state_sub = nh.subscribe<prometheus_msgs::DroneState>("/prometheus/drone_state", 10, drone_state_cb);
+
+    ros::Publisher message_pub = nh.advertise<prometheus_msgs::Message>("/prometheus/message/main", 10);
+
+    //【发布】发送给控制模块 [px4_pos_controller.cpp]的命令
+    ros::Publisher command_pub = nh.advertise<prometheus_msgs::ControlCommand>("/prometheus/control_command", 10);
+
+    Command_Now.Command_ID = 1;
+    Command_Now.source = NODE_NAME;
+
+    bool sim_mode = true;
+    if(sim_mode)
+    {
+        // Waiting for input
+        int start_flag = 0;
+        while(start_flag == 0)
+        {
+            cout << ">>>>>>>>>>>>>>>>>>>>>>>>>Gimbal Tracking Mission<<<<<<<<<<<<<<<<<<<<<< "<< endl;
+            cout << "Please check the parameter and setting，enter 1 to continue， else for quit: "<<endl;
+            cin >> start_flag;
+        }
+
+        while(ros::ok() && _DroneState.mode != "OFFBOARD")
+        {
+            Command_Now.header.stamp = ros::Time::now();
+            Command_Now.Mode  = prometheus_msgs::ControlCommand::Idle;
+            Command_Now.Command_ID = Command_Now.Command_ID + 1;
+            Command_Now.source = NODE_NAME;
+            Command_Now.Reference_State.yaw_ref = 999;
+            command_pub.publish(Command_Now);   
+            cout << "Switch to OFFBOARD and arm ..."<<endl;
+            ros::Duration(2.0).sleep();
+            ros::spinOnce();
+        }
+    }
+
+
+    // 起飞
+    pub_message(message_pub, prometheus_msgs::Message::WARN, NODE_NAME, "Takeoff to predefined position.");
+
+    while( _DroneState.position[2] < 0.5)
+    {      
+        Command_Now.header.stamp                        = ros::Time::now();
+        Command_Now.Mode                                = prometheus_msgs::ControlCommand::Move;
+        Command_Now.Command_ID                          = Command_Now.Command_ID + 1;
+        Command_Now.source                              = NODE_NAME;
+        Command_Now.Reference_State.Move_mode           = prometheus_msgs::PositionReference::XYZ_POS;
+        Command_Now.Reference_State.Move_frame          = prometheus_msgs::PositionReference::ENU_FRAME;
+        Command_Now.Reference_State.position_ref[0]     = start_point[0];
+        Command_Now.Reference_State.position_ref[1]     = start_point[1];
+        Command_Now.Reference_State.position_ref[2]     = start_point[2];
+        Command_Now.Reference_State.yaw_ref             = 0.0;
+        command_pub.publish(Command_Now);
+        cout << "Takeoff to start point..."<<endl;
+        ros::Duration(3.0).sleep();
+
+        ros::spinOnce();
+    }
+
+    // 等待
+    ros::Duration(3.0).sleep();
+
+    float time_trajectory = 0.0;
+    float trajectory_total_time = 10.0;
+
+    while(ros::ok())
+    {      
+        cout << ">>>>>>>>>>>>>>>>>>>>>>>>>Gimbal Tracking Mission<<<<<<<<<<<<<<<<<<<<<< "<< endl;
+        cout << "Please enter time：[s]: "<<endl;
+        cin >> trajectory_total_time;
+
+        time_trajectory = 0.0;
+
+        // 追圆形
+        while(time_trajectory < trajectory_total_time)
+        {
+            Command_Now.header.stamp = ros::Time::now();
+            Command_Now.Mode = prometheus_msgs::ControlCommand::Move;
+            Command_Now.Command_ID = Command_Now.Command_ID + 1;
+            Command_Now.source = NODE_NAME;
+
+            Command_Now.Reference_State = Circle_trajectory_generation(time_trajectory);
+
+            command_pub.publish(Command_Now);
+            time_trajectory = time_trajectory + 0.05;
+
+            cout << "Trajectory tracking: "<< time_trajectory << " / " << trajectory_total_time  << " [ s ]" <<endl;
+
+            gimbal_att = gimbal_control_.get_gimbal_att();
+            gimbal_att_deg = gimbal_att/PI*180;
+            cout << "gimbal_att    : " << gimbal_att_deg[0] << " [deg] "<< gimbal_att_deg[1] << " [deg] "<< gimbal_att_deg[2] << " [deg] "<<endl;
+            cout << "gimbal_att_sp : " << gimbal_att_sp_deg[0] << " [deg] "<< gimbal_att_sp_deg[1] << " [deg] "<< gimbal_att_sp_deg[2] << " [deg] "<<endl;
+            
+            gimbal_control_.send_mount_control_command(gimbal_att_sp_deg);
+
+            printf_result();
+            ros::spinOnce();
+            ros::Duration(0.05).sleep();
+        }
+
+        printf_result();
+        ros::spinOnce();
+
+        rate.sleep();
+    }
+
+    return 0;
+
+}
+
+void printf_result()
+{
+    //固定的浮点显示
+    cout.setf(ios::fixed);
+    //setprecision(n) 设显示小数精度为n位
+    cout<<setprecision(2);
+    //左对齐
+    cout.setf(ios::left);
+    // 强制显示小数点
+    cout.setf(ios::showpoint);
+    // 强制显示符号
+    cout.setf(ios::showpos);
+}
+
+prometheus_msgs::PositionReference Circle_trajectory_generation(float time_from_start)
+{
+    prometheus_msgs::PositionReference Circle_trajectory;
+    float omega;
+    if( circle_radius != 0)
+    {
+        omega = direction * fabs(linear_vel / circle_radius);
+    }else
+    {
+        omega = 0.0;
+    }
+    const float angle = time_from_start * omega;
+    const float cos_angle = cos(angle);
+    const float sin_angle = sin(angle);
+
+    // cout << "omega : " << omega  * 180/M_PI <<" [deg/s] " <<endl;
+    // cout << "angle : " << angle  * 180/M_PI <<" [deg] " <<endl;
+
+    Circle_trajectory.header.stamp = ros::Time::now();
+
+    Circle_trajectory.time_from_start = time_from_start;
+
+    Circle_trajectory.Move_mode = prometheus_msgs::PositionReference::TRAJECTORY;
+
+    Circle_trajectory.position_ref[0] = circle_radius * cos_angle + circle_center[0];
+    Circle_trajectory.position_ref[1] = circle_radius * sin_angle + circle_center[1];
+    Circle_trajectory.position_ref[2] = circle_center[2];
+
+    Circle_trajectory.velocity_ref[0] = - circle_radius * omega * sin_angle;
+    Circle_trajectory.velocity_ref[1] = circle_radius * omega * cos_angle;
+    Circle_trajectory.velocity_ref[2] = 0;
+
+    Circle_trajectory.acceleration_ref[0] = - circle_radius * pow(omega, 2.0) * cos_angle;
+    Circle_trajectory.acceleration_ref[1] = - circle_radius * pow(omega, 2.0) * sin_angle;
+    Circle_trajectory.acceleration_ref[2] = 0;
+
+    // Circle_trajectory.jerk_ref[0] = circle_radius * pow(omega, 3.0) * sin_angle;
+    // Circle_trajectory.jerk_ref[1] = - circle_radius * pow(omega, 3.0) * cos_angle;
+    // Circle_trajectory.jerk_ref[2] = 0;
+
+    // Circle_trajectory.snap_ref[0] = circle_radius * pow(omega, 4.0) * cos_angle;
+    // Circle_trajectory.snap_ref[1] = circle_radius * pow(omega, 4.0) * sin_angle;
+    // Circle_trajectory.snap_ref[2] = 0;
+
+    Circle_trajectory.yaw_ref = 0;
+    // Circle_trajectory.yaw_rate_ref = 0;
+    // Circle_trajectory.yaw_acceleration_ref = 0;
+
+    return Circle_trajectory;
+}
