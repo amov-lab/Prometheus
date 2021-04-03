df767c4e20f51655c2ec6c82520d9aeee980565f
jario <jariof@foxmail.com>
2021-04-03 18:40:45 +0800

    detection: add some helper script

---
+++ Scripts/download_detection_submodules.sh
@@ -0,0 +1,3 @@
+# download submodule: prometheus_landing_detection
+git clone https://gitee.com/jario-jin/prometheus_landing_detection.git Modules/object_detection_landing
+# git clone https://gitee.com/jario-jin/prometheus_data_persistence.git Modules/object_detection_persistence
---
+++ Scripts/install_detection_yolov5openvino.sh
@@ -0,0 +1,6 @@
+# download submodule: prometheus_landing_detection
+git clone https://gitee.com/jario-jin/yolov5-openvino.git Modules/object_detection_yolov5openvino
+cd Modules/object_detection_yolov5openvino
+pip3 install -r requirements.txt
+
+
