22318ea8b28edcf3a6306b78e34e1f3570addd5a
jario <jariof@foxmail.com>
2021-04-03 16:44:52 +0800

    detection: add yolov5 intel openvino node

--- compile_detection.sh
+++ compile_detection.sh
@@ -11,3 +11,12 @@ else
   # compile object_detection_landing
   catkin_make --source Modules/object_detection_landing --build build/object_detection_landing
 fi
+
+if [ ! -f "Modules/object_detection_yolov5openvino/CMakeLists.txt" ]; then
+  # submodule object_detection_yolov5openvino not exist, skip it
+  echo -e "\e[32m[INFO] SUBMODULE\e[0m \e[33mobject_detection_yolov5openvino\e[0m \e[32mNOT EXIST, Skip it!\e[0m"
+else
+  echo -e "\e[32m[INFO] COMPILE \e[33mobject_detection_yolov5openvino\e[0m \e[32m...\e[0m"
+  # compile object_detection_landing
+  catkin_make --source Modules/object_detection_yolov5openvino --build build/object_detection_yolov5openvino
+fi
--- download_detection_submodules.sh
+++ download_detection_submodules.sh
@@ -1,4 +1,4 @@
 # download submodule: prometheus_landing_detection
 git clone https://gitee.com/jario-jin/prometheus_landing_detection.git Modules/object_detection_landing
 # git clone https://gitee.com/jario-jin/prometheus_data_persistence.git Modules/object_detection_persistence
-git clone https://gitee.com/jario-jin/prometheus_yolov5_openvino.git Modules/object_detection_yolov5openvino
+# git clone https://gitee.com/jario-jin/prometheus_yolov5_openvino.git Modules/object_detection_yolov5openvino
