83661dbff32381f641456ffd4c477739aadbbee4
Ren Jin <jariof@foxmail.com>
2021-04-07 19:46:00 +0800

    Update install_detection_yolov5openvino.sh

--- Scripts/install_detection_yolov5openvino.sh
+++ Scripts/install_detection_yolov5openvino.sh
@@ -1,4 +1,4 @@
-# download submodule: prometheus_landing_detection
+# download submodule: object_detection_yolov5openvino
 git clone https://gitee.com/jario-jin/yolov5-openvino.git Modules/object_detection_yolov5openvino
 cd Modules/object_detection_yolov5openvino
 pip3 install -r requirements.txt
