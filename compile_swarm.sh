7896d0c573115f8eeb49b79ee8d4ffb3eaf88032
qi <fatmoonqyp@126.com>
2021-05-13 19:50:18 +0800

    update compile_swarm.sh

--- compile_swarm.sh
+++ compile_swarm.sh
@@ -1,4 +1,3 @@
 catkin_make --source Modules/common/msgs --build build/msgs
-catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/ground_station --build build/ground_station
 catkin_make --source Modules/swarm_control --build build/swarm_control
