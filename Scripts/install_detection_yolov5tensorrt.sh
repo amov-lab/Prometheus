993020f2d5d1f9a88f1353bc3fceb557cf127d49
jario <jariof@foxmail.com>
2021-05-19 08:48:35 +0800

    detection: add yolov5 tensor-RT

---
+++ Scripts/install_detection_yolov5tensorrt.sh
@@ -0,0 +1,6 @@
+# download submodule: object_detection_yolov5openvino
+git clone https://gitee.com/jario-jin/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
+cd Modules/object_detection_yolov5tensorrt
+pip3 install -r requirements.txt
+
+
