61a222fc8b7ae4ad7ec5c2861f13d5815d214381
qi-x1e <fatmoonqyp@126.com>
2021-06-25 15:34:26 +0800

    add ego_planner_swarm

--- compile_ego.sh
+++ compile_ego.sh
@@ -1,5 +1,6 @@
 catkin_make --source Modules/common/msgs --build build/msgs
-catkin_make --source Modules/px4_ego --build build/px4_ego
+catkin_make --source Modules/ego_planner --build build/ego_planner
+catkin_make --source Modules/swarm_control --build build/swarm_control
 catkin_make --source Modules/ground_station --build build/ground_station
 catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
 
