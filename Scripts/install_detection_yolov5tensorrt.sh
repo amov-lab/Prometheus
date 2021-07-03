69a61f6a1b59648f808a465653a3996935237e18
jario <jariof@foxmail.com>
2021-07-03 18:21:26 +0800

    detection: fix yolov5tensorrt install bugs

--- Scripts/install_detection_yolov5tensorrt.sh
+++ Scripts/install_detection_yolov5tensorrt.sh
@@ -1,6 +1,10 @@
 # download submodule: object_detection_yolov5openvino
 git clone https://gitee.com/jario-jin/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
 cd Modules/object_detection_yolov5tensorrt
-pip3 install -r requirements.txt
-
-
+pip3 install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+mkdir build
+cd build
+cmake ..
+make
+python3 gen_wts.py
+sudo ./yolov5 -s
