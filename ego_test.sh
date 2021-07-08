fbe80238f0c20d3a9073508fb12f534e3c068cfb
qi <fatmoonqyp@126.com>
2021-07-08 22:06:24 +0800

    update xxxxx

--- ego_test.sh
+++ ego_test.sh
@@ -2,11 +2,15 @@
 # source /home/dji/Kongdijiqun/devel/setup.bash
 
 # 启动gazebo,rviz
-roslaunch prometheus_gazebo sitl_ego_map4.launch & sleep 10s;
+roslaunch prometheus_gazebo sitl_ego_map4.launch & sleep 8s;
 # PX4 sitl
-roslaunch prometheus_gazebo sitl_ego_px4.launch & sleep 5s;
+roslaunch prometheus_gazebo sitl_ego_px4_uav1.launch & sleep 4s;
+# PX4 sitl
+# roslaunch prometheus_gazebo sitl_ego_px4_uav2.launch & sleep 4s;
 # ego
-roslaunch prometheus_gazebo sitl_ego_planner.launch & sleep 3s;
+roslaunch prometheus_gazebo sitl_ego_planner_uav1.launch & sleep 2s;
+# ego
+# roslaunch prometheus_gazebo sitl_ego_planner_uav2.launch & sleep 2s;
 # case3 station
 roslaunch prometheus_gazebo sitl_ego_station.launch
 
