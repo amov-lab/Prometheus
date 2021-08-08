d4b0cd42f855ad206023c34e2fb9bc03d8f89ee5
qi <fatmoonqyp@126.com>
2021-08-08 23:46:53 +0800

    update swarm apf

--- compile_all.sh
+++ compile_all.sh
@@ -7,6 +7,8 @@ catkin_make --source Modules/planning --build build/planning
 catkin_make --source Modules/ground_station --build build/ground_station
 catkin_make --source Modules/ugv_control --build build/ugv_control
 catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
+catkin_make --source Modules/ego_planner --build build/ego_planner
+catkin_make --source Modules/swarm_control --build build/swarm_control
 catkin_make --source Experiment --build build/prometheus_experiment
 
 
