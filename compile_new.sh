ff3e71996a763330fe07254765a05085826ed613
amov <fatmoonqyp@126.com>
2022-01-14 02:22:32 +0800

    add matlab bridge

--- compile_new.sh
+++ compile_new.sh
@@ -1,4 +1,4 @@
 catkin_make --source Modules/common/msgs --build build/msgs
-catkin_make --source Modules/ego_planner --build build/ego_planner
 catkin_make --source Modules/uav_control --build build/uav_control
+catkin_make --source Modules/matlab_bridge --build build/matlab_bridge
 
