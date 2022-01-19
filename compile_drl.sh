c60d47234b9451a8630a2dc506ca1647ccb18392
QI-SYSU <fatmoonqyp@126.com>
2022-01-19 15:03:34 +0800

    update drl cmakelist

--- compile_drl.sh
+++ compile_drl.sh
@@ -1,3 +1,2 @@
 catkin_make --source Modules/common/msgs --build build/msgs
-catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
 catkin_make --source Modules/deep_rl --build build/deep_rl
