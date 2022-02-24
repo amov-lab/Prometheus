51fd036c911baa7ff3d739687afac7340203d47b
qi <fatmoonqyp@126.com>
2022-02-24 23:37:19 +0800

    path planning init

---
+++ Modules/path_planning/Utils/cmake_utils/cmake/cmake_modules.cmake
@@ -0,0 +1,2 @@
+list(APPEND CMAKE_MODULE_PATH "${cmake_utils_SOURCE_PREFIX}/cmake_modules")
+link_directories( ${cmake_utils_SOURCE_PREFIX}/lib/mosek8 )
--- compile_new.sh
+++ compile_new.sh
@@ -2,4 +2,5 @@ catkin_make --source Modules/common/msgs --build build/msgs
 catkin_make --source Modules/uav_control --build build/uav_control
 catkin_make --source Modules/matlab_bridge --build build/matlab_bridge
 catkin_make --source Modules/swarm_control --build build/swarm_control
+catkin_make --source Modules/path_planning --build build/path_planning
 
