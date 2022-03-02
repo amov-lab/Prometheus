7b0ef660a92cc7e6f3b1711523d7225d349a191a
Eason Yi <eason473867143@gmail.com>
2022-03-02 18:21:14 +0800

    uav_control test ok

--- compile_new.sh
+++ compile_new.sh
@@ -1,5 +1,2 @@
 catkin_make --source Modules/common/msgs --build build/msgs
 catkin_make --source Modules/uav_control --build build/uav_control
-catkin_make --source Modules/demo --build build/demo
-catkin_make --source Modules/matlab_bridge --build build/matlab_bridge
-
