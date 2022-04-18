7c17cbf9a65048a155d41e0255f86578d8b0ceca
qi <fatmoonqyp@126.com>
2022-04-19 01:21:20 +0800

    fix mavros config bug

--- Modules/uav_control/src/uav_controller.cpp
+++ Modules/uav_control/src/uav_controller.cpp
@@ -1186,8 +1186,8 @@ void UAV_controller::debug_cb(const ros::TimerEvent &e)
 
     cout << "[ " << uav_state.mode<<" ] " << TAIL <<endl;
 
-    cout << GREEN << "Battery Voltage   : " << uav_state.battery_state<<" [V] " << TAIL <<endl;
-    cout << GREEN << "Battery Percentage: " << uav_state.battery_percetage<<" % " << TAIL <<endl;
+    cout << GREEN  << "Battery Voltage : " << uav_state.battery_state<<" [V] " << TAIL <<endl;
+    cout << GREEN  << "Battery Percent : " << uav_state.battery_percetage << TAIL <<endl;
     cout << GREEN  << "UAV_pos [X Y Z] : " << uav_pos[0] << " [ m ] "<< uav_pos[1]<<" [ m ] "<<uav_pos[2]<<" [ m ] "<< TAIL <<endl;
     cout << GREEN  << "UAV_vel [X Y Z] : " << uav_vel[0] << " [m/s] "<< uav_vel[1]<<" [m/s] "<<uav_vel[2]<<" [m/s] "<< TAIL <<endl;
     cout << GREEN  << "UAV_att [R P Y] : " << uav_state.attitude[0] * 180/M_PI <<" [deg] "<<uav_state.attitude[1] * 180/M_PI << " [deg] "<< uav_state.attitude[2] * 180/M_PI<<" [deg] "<< TAIL<<endl;
