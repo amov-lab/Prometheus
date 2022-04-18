53ceffdec87e506989302ebee06ca0ba2c6d9ec9
QI-SYSU <fatmoonqyp@126.com>
2022-04-18 22:31:42 +0800

    add rc_test

---
+++ Modules/uav_control/utils/rc_test.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+
+#include "rc_input.h"
+#include <mavros_msgs/RCIn.h>
+
+RC_Input rc_input;
+
+void px4_rc_cb(const mavros_msgs::RCIn::ConstPtr &msg)
+{
+    // 调用外部函数对遥控器数据进行处理，具体见rc_data.h
+    rc_input.handle_rc_data(msg, false);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "rc_test");
+    ros::NodeHandle nh("~");
+
+    rc_input.init(false);
+
+    int uav_id = 1;
+
+    //【订阅】PX4遥控器数据
+    ros::Subscriber px4_rc_sub =
+        nh.subscribe<mavros_msgs::RCIn>("/uav" + std::to_string(uav_id) + "/mavros/rc/in",
+                                        1,
+                                        px4_rc_cb);
+
+    // 检查PX4连接状态
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        rc_input.printf_info();
+        ros::Duration(0.05).sleep();
+    }
+}
