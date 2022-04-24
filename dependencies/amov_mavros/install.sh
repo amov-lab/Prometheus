51f16fd383bde999ed983a4962b1fc52b21528d0
onxming <betterming.wang@gmail.com>
2022-04-24 10:51:25 +0800

    add source condition

--- dependencies/amov_mavros/install.sh
+++ dependencies/amov_mavros/install.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-sudo apt update
-sudo apt-get install python-catkin-tools python-rosinstall-generator -y
-# For Noetic use that:
-# sudo apt install python3-catkin-tools python3-rosinstall-generator python3-osrf-pycommon -y
+sudo apt update &&\
+sudo apt-get install python-catkin-tools python-rosinstall-generator -y &&\
 
-# 1. Create the workspace: unneeded if you already has workspace
-mkdir -p ~/prometheus_mavros/src
-cd ~/prometheus_mavros
-catkin init
+mkdir -p ~/prometheus_mavros/src &&\
+cd ~/prometheus_mavros &&\
+catkin init &&\
 wstool init src
 
-# 4. Create workspace & deps
-wstool merge -t src $SHELL_FOLDER/mavros.rosinstall
-wstool update -t src -j4
-rosdep install --from-paths src --ignore-src -y
+wstool merge -t src $SHELL_FOLDER/mavros.rosinstall &&\
+wstool update -t src -j$(nproc) &&\
+rosdep install --from-paths src --ignore-src -y &&\
 
-# 5. Install GeographicLib datasets:
-./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
+./src/mavros/mavros/scripts/install_geographiclib_datasets.sh &&\
 
-# 6. Build source
-catkin build
+catkin build &&\
+source $HOME/prometheus_mavros/devel/setup.bash &&\
 
-# 7. Make sure that you use setup.bash or setup.zsh from workspace.
-#    Else rosrun can't find nodes from this workspace.
-source devel/setup.bash
-
-# 8. 加入.bashrc
-echo "source ${SHELL_FOLDER}/devel/setup.bash" >> ~/.bashrc
-
-
-
-
+source_command="source $HOME/prometheus_mavros/devel/setup.bash"
+if [ `grep -c "$source_command" ~/.bashrc` -eq '0' ];then
+    echo $source_command >> ~/.bashrc
+fi
