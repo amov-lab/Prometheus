b3015390e52e097f150ce1db402164330d1eb171
onx <betterming.wang@gmail.com>
2022-05-05 15:30:27 +0800

    add X86 and NX yolov5

--- Scripts/install_detection_yolov5tensorrt.sh
+++ Scripts/install_detection_yolov5tensorrt.sh
@@ -1,5 +1,5 @@
 # download submodule: object_detection_yolov5openvino
-git clone https://gitee.com/jario-jin/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
+git clone https://gitee.com/amovlab/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
 cd Modules/object_detection_yolov5tensorrt
 pip3 install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 mkdir build
