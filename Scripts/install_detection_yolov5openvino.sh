be286226dd4fc15015bf4966f6149b6db3080925
qi <fatmoonqyp@126.com>
2022-05-23 20:03:30 +0800

    change mavros install sh place

--- Scripts/install_detection_yolov5openvino.sh
+++
@@ -1,6 +0,0 @@
-# download submodule: object_detection_yolov5openvino
-git clone https://gitee.com/jario-jin/yolov5-openvino.git Modules/object_detection_yolov5openvino
-cd Modules/object_detection_yolov5openvino
-pip3 install -r requirements.txt
-
-
