49d3ab6972dc9f5a14280a66fb9a136ea8dbf79a
qi <fatmoonqyp@126.com>
2022-05-24 13:59:43 +0800

    Auto stash before merge of "prometheus-dev" and "gitee_amovlab/prometheus-dev"

--- Modules/motion_planning/global_planner/include/global_planner.h
+++ Modules/motion_planning/global_planner/include/global_planner.h
@@ -16,6 +16,7 @@
 
 #include <prometheus_msgs/UAVState.h>
 #include <prometheus_msgs/UAVCommand.h>
+#include <prometheus_msgs/UAVControlState.h>
 
 #include "A_star.h"
 #include "occupy_map.h"
@@ -34,14 +35,14 @@ namespace GlobalPlannerNS
     private:
         // 参数
         int uav_id;
+        bool sim_mode;
+        int map_input_source;
         double fly_height;
         double safe_distance;
         double time_per_path;
-        int map_input_source;
         double replan_time;
         bool consider_neighbour;
-        bool sim_mode;
-
+        
         // 订阅无人机状态、目标点、传感器数据（生成地图）
         ros::Subscriber goal_sub;
         ros::Subscriber uav_state_sub;
@@ -50,7 +51,7 @@ namespace GlobalPlannerNS
         ros::Subscriber Gpointcloud_sub;
         ros::Subscriber Lpointcloud_sub;
         ros::Subscriber laserscan_sub;
-
+        ros::Subscriber uav_control_state_sub;
         // 发布控制指令
         ros::Publisher uav_cmd_pub;
         ros::Publisher path_cmd_pub;
@@ -61,18 +62,16 @@ namespace GlobalPlannerNS
         Astar::Ptr Astar_ptr;
 
         prometheus_msgs::UAVState uav_state;      // 无人机状态
+        prometheus_msgs::UAVControlState uav_control_state;
         nav_msgs::Odometry uav_odom;
         Eigen::Vector3d uav_pos;  // 无人机位置
         Eigen::Vector3d uav_vel;  // 无人机速度
         Eigen::Quaterniond uav_quat; // 无人机四元数
         double uav_yaw;
-
+        // 规划终端状态
+        Eigen::Vector3d goal_pos, goal_vel;
         prometheus_msgs::UAVCommand uav_command; 
-
         nav_msgs::Path path_cmd;
-        double distance_walked;
-        
-
         double distance_to_goal;
 
         // 规划器状态
@@ -86,10 +85,6 @@ namespace GlobalPlannerNS
         int start_point_index;
         int Num_total_wp;
         int cur_id;
-
-        // 规划初始状态及终端状态
-        Eigen::Vector3d start_pos, start_vel, start_acc, goal_pos, goal_vel;
-
         float desired_yaw;
 
         ros::Time tra_start_time;
@@ -111,17 +106,12 @@ namespace GlobalPlannerNS
         void Gpointcloud_cb(const sensor_msgs::PointCloud2ConstPtr &msg);
         void Lpointcloud_cb(const sensor_msgs::PointCloud2ConstPtr &msg);
         void laser_cb(const sensor_msgs::LaserScanConstPtr &msg);
-
+        void uav_control_state_cb(const prometheus_msgs::UAVControlState::ConstPtr &msg);
         void safety_cb(const ros::TimerEvent &e);
         void mainloop_cb(const ros::TimerEvent &e);
         void track_path_cb(const ros::TimerEvent &e);
-
-        // 【获取当前时间函数】 单位：秒
-        float get_time_in_sec(const ros::Time &begin_time);
-
+        void debug_info();
         int get_start_point_id(void);
-
-
     };
 
 } // namespace GlobalPlannerNS
