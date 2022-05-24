4369cfeea355bab812199723d05172a06db1d241
onx <betterming.wang@gmail.com>
2022-05-24 11:26:20 +0800

    update siamrpn readme, add yolov5 demo

--- Scripts/install_detection_yolov5tensorrt.sh
+++ Scripts/install_detection_yolov5tensorrt.sh
@@ -1,10 +1,10 @@
 # download submodule: object_detection_yolov5openvino
-git clone https://gitee.com/amovlab/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
+git clone --branch x86 https://gitee.com/amovlab/prometheus-yolov5-tensorrt.git Modules/object_detection_yolov5tensorrt
 cd Modules/object_detection_yolov5tensorrt
 pip3 install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 mkdir build
 cd build
 cmake ..
 make
-python3 gen_wts.py
+python3 ../gen_wts.py
 sudo ./yolov5 -s
