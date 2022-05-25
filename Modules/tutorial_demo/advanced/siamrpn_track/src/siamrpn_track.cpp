219df5835b25b95fc7fc11d41512a67a059bf049
onx <betterming.wang@gmail.com>
2022-05-25 17:43:14 +0800

    upgrade printf_uitls.h, optimization siamrpn_track

--- Modules/tutorial_demo/advanced/siamrpn_track/src/siamrpn_track.cpp
+++ Modules/tutorial_demo/advanced/siamrpn_track/src/siamrpn_track.cpp
@@ -7,8 +7,11 @@
 // topic 头文件
 #include <prometheus_msgs/UAVCommand.h>
 #include <prometheus_msgs/UAVState.h>
+#include <prometheus_msgs/UAVControlState.h>
 #include <prometheus_msgs/DetectionInfo.h>
 #include "message_utils.h"
+
+#include "printf_utils.h"
 
 using namespace std;
 using namespace Eigen;
@@ -17,7 +20,8 @@ using namespace Eigen;
 prometheus_msgs::UAVState g_UAVState;
 Eigen::Vector3f g_drone_pos;
 //---------------------------------------Vision---------------------------------------------
-prometheus_msgs::DetectionInfo g_Detection_raw; //目标位置[机体系下：前方x为正，右方y为正，下方z为正]
+prometheus_msgs::DetectionInfo g_Detection_raw;      //目标位置[机体系下：前方x为正，右方y为正，下方z为正]
+prometheus_msgs::UAVControlState g_uavcontrol_state; //目标位置[机体系下：前方x为正，右方y为正，下方z为正]
 Eigen::Vector3f pos_body_frame;
 Eigen::Vector3f pos_body_enu_frame;    //原点位于质心，x轴指向前方，y轴指向左，z轴指向上的坐标系
 float kpx_track, kpy_track, kpz_track; //控制参数 - 比例参数
@@ -26,16 +30,13 @@ bool is_detected = false;        // 是否检测到目标标志
 int num_count_vision_lost = 0;   //视觉丢失计数器
 int num_count_vision_regain = 0; //视觉丢失计数器
 int g_uav_id;
-int Thres_vision = 0;            //视觉丢失计数器阈值
+int Thres_vision = 0; //视觉丢失计数器阈值
 Eigen::Vector3f camera_offset;
 //---------------------------------------Track---------------------------------------------
 float distance_to_setpoint;
 Eigen::Vector3f tracking_delta;
 //---------------------------------------Output---------------------------------------------
 prometheus_msgs::UAVCommand g_command_now; //发送给控制模块 [px4_pos_controller.cpp]的命令
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>声 明 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-void printf_param();  //打印各项参数以供检查
-void printf_result(); //打印函数
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>回 调 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 void vision_cb(const prometheus_msgs::DetectionInfo::ConstPtr &msg)
 {
@@ -82,12 +83,14 @@ void drone_state_cb(const prometheus_msgs::UAVState::ConstPtr &msg)
     g_drone_pos[1] = g_UAVState.position[1];
     g_drone_pos[2] = g_UAVState.position[2];
 }
+
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>主 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "object_tracking");
     ros::NodeHandle nh("~");
 
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>参数读取<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //节点运行频率： 20hz 【视觉端解算频率大概为20HZ】
     ros::Rate rate(20.0);
 
@@ -113,108 +116,58 @@ int main(int argc, char **argv)
     nh.param<float>("start_point_z", start_point_z, 2.0);
 
     nh.param<int>("uav_id", g_uav_id, 1);
-
-    // 【订阅】视觉消息 来自视觉节点
-    //  方向定义： 识别算法发布的目标位置位于相机坐标系（从相机往前看，物体在相机右方x为正，下方y为正，前方z为正）
-    //  标志位：   detected 用作标志位 ture代表识别到目标 false代表丢失目标
-    // 注意这里为了复用程序使用了/prometheus/object_detection/kcf_tracker作为话题名字，适用于椭圆、二维码、yolo等视觉算法
-    // 故同时只能运行一种视觉识别程序，如果想同时追踪多个目标，这里请修改接口话题的名字
-    ros::Subscriber vision_sub = nh.subscribe<prometheus_msgs::DetectionInfo>("/uav" + std::to_string(g_uav_id) + "/prometheus/object_detection/siamrpn_tracker", 10, vision_cb);
-
-    ros::Subscriber drone_state_sub = nh.subscribe<prometheus_msgs::UAVState>("/uav" + std::to_string(g_uav_id) + "/prometheus/state", 10, drone_state_cb);
-
-    // 【发布】发送给控制模块 [px4_pos_controller.cpp]的命令
-    ros::Publisher command_pub = nh.advertise<prometheus_msgs::UAVCommand>("/uav" + std::to_string(g_uav_id) + "/prometheus/command", 10);
-
-    // 【发布】用于地面站显示的提示消息
-    ros::Publisher message_pub = nh.advertise<prometheus_msgs::Message>("/uav" + std::to_string(g_uav_id) + "/prometheus/message/main", 10);
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>参数读取<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     nh.param<float>("start_yaw", start_yaw, 0.0);
 
-    //打印现实检查参数
-    printf_param();
-    //固定的浮点显示
-    cout.setf(ios::fixed);
-    // setprecision(n) 设显示小数精度为n位
-    cout << setprecision(2);
-    //左对齐
-    cout.setf(ios::left);
-    // 强制显示小数点
-    cout.setf(ios::showpoint);
-    // 强制显示符号
-    cout.setf(ios::showpos);
+    // 订阅视觉反馈
+    ros::Subscriber vision_sub = nh.subscribe<prometheus_msgs::DetectionInfo>("/uav" + std::to_string(g_uav_id) + "/prometheus/object_detection/siamrpn_tracker", 10, vision_cb);
+    // 订阅当前无人机控制状态, 使用lambda匿名函数
+    ros::Subscriber uav_control_state_sub = nh.subscribe<prometheus_msgs::UAVControlState>("/uav" + std::to_string(g_uav_id) + "/prometheus/control_state", 10, [&](const prometheus_msgs::UAVControlState::ConstPtr &msg) -> void
+                                                                                           { g_uavcontrol_state = *msg; });
+    // 订阅无人机状态
+    ros::Subscriber drone_state_sub = nh.subscribe<prometheus_msgs::UAVState>("/uav" + std::to_string(g_uav_id) + "/prometheus/state", 10, drone_state_cb);
 
-    // Waiting for input
-    int start_flag = 0;
-    while (start_flag == 0)
-    {
-        cout << ">>>>>>>>>>>>>>>>>>>>>>>>>Object Tracking Mission<<<<<<<<<<<<<<<<<<<<<< " << endl;
-        cout << "Please check the parameter and setting，enter 1 to continue， else for quit: " << endl;
-        cin >> start_flag;
-    }
+    // 发布控制指令
+    ros::Publisher command_pub = nh.advertise<prometheus_msgs::UAVCommand>("/uav" + std::to_string(g_uav_id) + "/prometheus/command", 10);
 
-    // 起飞
-    cout << "[object tracking]: "
-         << "Takeoff to predefined position." << endl;
-    g_command_now.Command_ID = 1;
-    while (g_UAVState.position[2] < 0.3)
-    {
-        g_command_now.header.stamp = ros::Time::now();
-        g_command_now.Agent_CMD = prometheus_msgs::UAVCommand::Move;
-        g_command_now.Command_ID = g_command_now.Command_ID + 1;
-        g_command_now.Move_mode = prometheus_msgs::UAVCommand::XYZ_POS;
-        g_command_now.position_ref[0] = start_point_x;
-        g_command_now.position_ref[1] = start_point_y;
-        g_command_now.position_ref[2] = start_point_z;
-        g_command_now.yaw_ref = start_yaw / 180 * 3.1415926;
-        command_pub.publish(g_command_now);
-        cout << "Takeoff ..." << endl;
-        ros::Duration(3.0).sleep();
-
-        ros::spinOnce();
-    }
-
+    // 初始化
     g_command_now.position_ref[0] = 0;
     g_command_now.position_ref[1] = 0;
     g_command_now.position_ref[2] = 0;
 
-    // 先读取一些飞控的数据
-    for (int i = 0; i < 10; i++)
-    {
-        ros::spinOnce();
-        rate.sleep();
-    }
-
-    ros::Duration(3.0).sleep();
-
     while (ros::ok())
     {
-        //回调
+        // 执行回调获取数据
         ros::spinOnce();
+        if (g_uavcontrol_state.control_state != prometheus_msgs::UAVControlState::COMMAND_CONTROL)
+        {
+            PCOUT(-1, TAIL, "Waiting for enter COMMAND_CONTROL state");
+            continue;
+        }
 
         g_command_now.header.stamp = ros::Time::now();
         g_command_now.Command_ID = g_command_now.Command_ID + 1;
 
-        printf_result();
-
-        // 对目标到直线距离
+        // 到目标的直线距离(估计值)
         distance_to_setpoint = pos_body_frame.norm();
+
         if (!is_detected)
         {
             g_command_now.Agent_CMD = prometheus_msgs::UAVCommand::Current_Pos_Hover;
-            cout << "[object_tracking]: Lost the Target " << endl;
-            pub_message(message_pub, prometheus_msgs::Message::WARN, NODE_NAME, "Lost the Target.");
+            PCOUT(-1, GREEN, "Waiting for detected object");
         }
         else
         {
-            cout << "[object_tracking]: Tracking the Target, distance_to_setpoint : " << distance_to_setpoint << " [m] " << endl;
             g_command_now.Agent_CMD = prometheus_msgs::UAVCommand::Move;
+            // 使用全速度控制
             g_command_now.Move_mode = prometheus_msgs::UAVCommand::XYZ_VEL; // xy velocity z position
 
+            // 根据误差计算计算应该给于无人机的速度
             g_command_now.velocity_ref[0] = kpx_track * (pos_body_enu_frame[0] - tracking_delta[0]);
             g_command_now.velocity_ref[1] = kpy_track * (pos_body_enu_frame[1] - tracking_delta[1]);
-            g_command_now.velocity_ref[2] = kpz_track * (tracking_delta[2] - g_UAVState.position[2] );
+            g_command_now.velocity_ref[2] = kpz_track * (tracking_delta[2] - g_UAVState.position[2]);
             g_command_now.yaw_ref = start_yaw / 180 * 3.1415926;
+            std::string tmp = "[object_tracking]: Tracking the Target, distance_to_setpoint : " + std::to_string(distance_to_setpoint) + " [m] " + "\n velocity_x: " + std::to_string(g_command_now.velocity_ref[0]) + "  [m/s], velocity_y: " + std::to_string(g_command_now.velocity_ref[1]) + " [m/s], velocity_y: " + std::to_string(g_command_now.velocity_ref[2]) + " [m/s]";
+            PCOUT(1, GREEN, tmp);
         }
 
         // Publish
@@ -226,57 +179,4 @@ int main(int argc, char **argv)
     }
 
     return 0;
-}
-
-void printf_result()
-{
-    //固定的浮点显示
-    cout.setf(ios::fixed);
-    // setprecision(n) 设显示小数精度为n位
-    cout << setprecision(4);
-    //左对齐
-    cout.setf(ios::left);
-    // 强制显示小数点
-    cout.setf(ios::showpoint);
-    // 强制显示符号
-    cout.setf(ios::showpos);
-
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>Obeject Tracking<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>Vision State<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-    if (is_detected)
-    {
-        cout << "is_detected: ture" << endl;
-    }
-    else
-    {
-        cout << "is_detected: false" << endl;
-    }
-
-    cout << "Detection_raw: " << g_Detection_raw.position[0] << " [m] " << g_Detection_raw.position[1] << " [m] " << g_Detection_raw.position[2] << " [m] " << endl;
-    cout << "Detection_raw: " << g_Detection_raw.attitude[2] / 3.1415926 * 180 << " [du] " << endl;
-
-    cout << "pos_body_frame: " << pos_body_frame[0] << " [m] " << pos_body_frame[1] << " [m] " << pos_body_frame[2] << " [m] " << endl;
-    cout << "pos_body_enu_frame: " << pos_body_enu_frame[0] << " [m] " << pos_body_enu_frame[1] << " [m] " << pos_body_enu_frame[2] << " [m] " << endl;
-
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>Land Control State<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-    cout << "volicity_des: " << g_command_now.velocity_ref[0] << " [m] " << g_command_now.velocity_ref[1] << " [m] " << g_command_now.velocity_ref[2] << " [m] " << endl;
-}
-
-void printf_param()
-{
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Parameter <<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-    cout << "Thres_vision : " << Thres_vision << endl;
-    cout << "uav id : " << g_uav_id << endl;
-
-    cout << "tracking_delta_x : " << tracking_delta[0] << endl;
-    cout << "tracking_delta_y : " << tracking_delta[1] << endl;
-    cout << "tracking_delta_z : " << tracking_delta[2] << endl;
-
-    cout << "kpx_track : " << kpx_track << endl;
-    cout << "kpy_track : " << kpy_track << endl;
-    cout << "kpz_track : " << kpz_track << endl;
-    cout << "start_point_x : " << start_point_x << endl;
-    cout << "start_point_y : " << start_point_y << endl;
-    cout << "start_point_z : " << start_point_z << endl;
 }
