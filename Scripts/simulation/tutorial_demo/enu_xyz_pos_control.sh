76c9f199a257df4960c27728d3d2f1b0e913a12d
BOSHEN97 <554123904@qq.com>
2022-06-13 20:18:15 +0800

    update demo sh

---
+++ Scripts/simulation/tutorial_demo/enu_xyz_pos_control.sh
@@ -0,0 +1,11 @@
+# 脚本名称: enu_xyz_pos_control
+# 脚本描述: 该脚本为本地坐标系下位置控制demo启动脚本,包含PX4 SITL,Gazebo仿真环境,无人机控制节点以及本地坐标系下位置控制demo
+
+# 启动PX4 SITL及Gazebo仿真环境
+roslaunch prometheus_gazebo sitl_outdoor_1uav.launch & sleep 5s;
+# 启动无人机控制 - uav_control_main
+roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 5s;
+# 启动demo (C语言版本)
+roslaunch prometheus_demo enu_xyz_pos_control.launch
+# 启动demo (python版本)
+# rosrun prometheus_demo enu_xyz_pos_control.py
--- Scripts/simulation/tutorial_demo/takeoff_land.sh
+++ Scripts/simulation/tutorial_demo/takeoff_land.sh
@@ -1,5 +1,5 @@
 # 脚本名称: takeoff_land
-# 脚本描述: todo
+# 脚本描述: 该脚本为起飞&降落控制demo启动脚本,包含PX4 SITL,Gazebo仿真环境,无人机控制节点以及起飞&降落控制demo
 
 # 启动PX4 SITL及Gazebo仿真环境
 roslaunch prometheus_gazebo sitl_outdoor_1uav.launch & sleep 5s;
@@ -9,6 +9,3 @@ roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 5s;
 roslaunch prometheus_demo takeoff_land.launch
 # 启动demo (python版本)
 # rosrun prometheus_demo takeoff_land.py
-
-
-
