4b97400db76d5f7e18883ef47835e2ec38561049
yhygg <1120809699@qq.com>
2022-06-13 14:11:48 +0800

    删除MATLAB_bridge

--- Modules/matlab_bridge/scripts/simulation_4uav.sh
+++
@@ -1,7 +0,0 @@
-
-# 启动Gazebo仿真环境, px4 sitl, mavros
-roslaunch prometheus_gazebo sitl_indoor_4uav.launch & sleep 5s;
-# 启动无人机控制
-roslaunch prometheus_uav_control uav_control_main_indoor_4uav.launch flag_printf:=false control/enable_external_control:=true & sleep 1s;
-# 启动matlab_bridge
-roslaunch prometheus_matlab matlab_bridge_4uav.launch 
