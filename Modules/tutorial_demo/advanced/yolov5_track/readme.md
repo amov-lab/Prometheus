3ce349e8a365ae4795074a952a27d6e2d76d65c4
onx <betterming.wang@gmail.com>
2022-06-16 10:57:08 +0800

    update shell

--- Modules/tutorial_demo/advanced/yolov5_track/readme.md
+++ Modules/tutorial_demo/advanced/yolov5_track/readme.md
@@ -7,7 +7,7 @@
 1. 安装`Modules/object_detection_yolov5tensorrt`，运行`Scripts/object_detection/install_detection_yolov5tensorrt.sh`。__注意载Prometheus根目录下运行__
 2. 启动
 ```bash
-roslaunch prometheus_demo yolov5_track.launch
+roslaunch prometheus_demo yolov5_track_all.launch
 # 另外开一个窗口运行进入Modules/object_detection_yolov5tensorrt路径
 # 注意在prometheus_python3环境下执行领命, 比如我到就是： (prometheus_python3) onx@onx:~$ python3 yolov5_tensorrt_client.py
 python3 yolov5_trt_ros.py  --image_topic /prometheus/sensor/monocular_front/image_raw
--- Scripts/simulation/tutorial_demo/find_aruco_marker.sh
+++ Scripts/simulation/tutorial_demo/find_aruco_marker.sh
@@ -2,4 +2,14 @@
 # 脚本描述： 无人机绕圆飞行，找相应的二维码，并前往相应二维码进行降落
 # 相关文档： Modules/tutorial_demo/advanced/find_aruco_marker/readme.md
 
-roslaunch prometheus_demo find_aruco_marker.launch
+# 启动PX4 SITL及Gazebo仿真环境
+roslaunch prometheus_demo sitl_outdoor_1uav_find_aruco_marker.launch & sleep 1s;
+# 启动无人机控制 - uav_control_main
+roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 1s;
+# 启动视觉模块
+roslaunch prometheus_demo aruco_datection.launch & sleep 1s;
+# 启动无人机控制
+roslaunch prometheus_demo find_aruco_marker.launch & sleep 1s;
+# 启动rqt_image_view,用于查看检测
+rqt_image_view
+
