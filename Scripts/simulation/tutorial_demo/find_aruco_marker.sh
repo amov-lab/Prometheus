3858537df892e913388d035c507220f9e24d1792
onx <betterming.wang@gmail.com>
2022-06-17 10:18:21 +0800

    Complete documentation

--- Scripts/simulation/tutorial_demo/find_aruco_marker.sh
+++ Scripts/simulation/tutorial_demo/find_aruco_marker.sh
@@ -3,13 +3,13 @@
 # 相关文档： Modules/tutorial_demo/advanced/find_aruco_marker/readme.md
 
 # 启动PX4 SITL及Gazebo仿真环境
-roslaunch prometheus_demo sitl_outdoor_1uav_find_aruco_marker.launch & sleep 1s;
+roslaunch prometheus_demo sitl_outdoor_1uav_find_aruco_marker.launch & sleep 2s;
 # 启动无人机控制 - uav_control_main
-roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 1s;
+roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 2s;
 # 启动视觉模块
-roslaunch prometheus_demo aruco_datection.launch & sleep 1s;
+roslaunch prometheus_demo aruco_datection.launch & sleep 2s;
 # 启动无人机控制
-roslaunch prometheus_demo find_aruco_marker.launch & sleep 1s;
+roslaunch prometheus_demo find_aruco_marker.launch & sleep 2s;
 # 启动rqt_image_view,用于查看检测
 rqt_image_view
 
