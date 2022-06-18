490bb6f401bd9b8b09488fd14a49bb83ecbad24a
BOSHEN97 <554123904@qq.com>
2022-06-18 10:07:39 +0800

    update sh

--- Scripts/simulation/motion_planning/astar_with_global_pcl.sh
+++ Scripts/simulation/motion_planning/astar_with_global_pcl.sh
@@ -1,14 +1,8 @@
 # 脚本名称: astar_with_global_pcl
 # 脚本描述: 单个无人机的astar算法测试(全局点云)
 
-# 启动Gazebo仿真环境 - Gazebo、rviz、map_generator_node
-roslaunch prometheus_simulator_utils map_generator.launch & sleep 5s;
-# 启动PX4 SITL 
-roslaunch prometheus_gazebo sitl_px4_indoor.launch uav_init_x:=-10 & sleep 3s;
-# 启动无人机控制 - uav_control_main_indoor
-roslaunch prometheus_uav_control uav_control_main_indoor.launch & sleep 3s;
-# 启动规划算法 
-roslaunch prometheus_global_planner sitl_global_planner_with_global_point.launch
-
-
-
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_simulator_utils map_generator.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch prometheus_gazebo sitl_px4_indoor.launch uav_init_x:=-10; exec bash"' \
+--tab -e 'bash -c "sleep 7; roslaunch prometheus_uav_control uav_control_main_indoor.launch; exec bash"' \
+--tab -e 'bash -c "sleep 8; roslaunch prometheus_global_planner sitl_global_planner_with_global_point.launch; exec bash"' \
--- Scripts/simulation/tutorial_demo/find_aruco_marker.sh
+++ Scripts/simulation/tutorial_demo/find_aruco_marker.sh
@@ -2,14 +2,9 @@
 # 脚本描述： 无人机绕圆飞行，找相应的二维码，并前往相应二维码进行降落
 # 相关文档： Modules/tutorial_demo/advanced/find_aruco_marker/readme.md
 
-# 启动PX4 SITL及Gazebo仿真环境
-roslaunch prometheus_demo sitl_outdoor_1uav_find_aruco_marker.launch & sleep 2s;
-# 启动无人机控制 - uav_control_main
-roslaunch prometheus_uav_control uav_control_main_outdoor.launch & sleep 2s;
-# 启动视觉模块
-roslaunch prometheus_demo aruco_datection.launch & sleep 2s;
-# 启动无人机控制
-roslaunch prometheus_demo find_aruco_marker.launch & sleep 2s;
-# 启动rqt_image_view,用于查看检测
-rqt_image_view
-
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_demo sitl_outdoor_1uav_find_aruco_marker.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch prometheus_uav_control uav_control_main_outdoor.launch; exec bash"' \
+--tab -e 'bash -c "sleep 7; roslaunch prometheus_demo aruco_datection.launch; exec bash"' \
+--tab -e 'bash -c "sleep 8; roslaunch prometheus_demo find_aruco_marker.launch; exec bash"' \
+--tab -e 'bash -c "sleep 9; rqt_image_view; exec bash"' \
