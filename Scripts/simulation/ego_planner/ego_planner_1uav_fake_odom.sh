93627dc0fa4e4c3933624ee980b09e4c617c1a5a
BOSHEN97 <554123904@qq.com>
2022-06-18 10:07:39 +0800

    update sh

--- Scripts/simulation/ego_planner/ego_planner_1uav_fake_odom.sh
+++ Scripts/simulation/ego_planner/ego_planner_1uav_fake_odom.sh
@@ -1,13 +1,10 @@
 # 脚本名称: ego_planner_1uav
 # 脚本描述: 单个无人机的ego_planner算法测试
 
-# 启动Gazebo仿真环境 - Gazebo、rviz、map_generator_node
-roslaunch prometheus_simulator_utils map_generator.launch swarm_num:=1 & sleep 5s;
-# 启动PX4 SITL 
-roslaunch prometheus_gazebo sitl_px4_indoor.launch uav_init_x:=-10 & sleep 5s;
-# 启动无人机控制 - uav_control_main_indoor
-roslaunch prometheus_uav_control uav_control_main_indoor.launch & sleep 4s;
-# 启动无人机规划 - ego_planner、traj_server
-roslaunch ego_planner sitl_ego_planner_basic.launch 
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_simulator_utils map_generator.launch swarm_num:=1; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch prometheus_gazebo sitl_px4_indoor.launch uav_init_x:=-10; exec bash"' \
+--tab -e 'bash -c "sleep 7; roslaunch prometheus_uav_control uav_control_main_indoor.launch; exec bash"' \
+--tab -e 'bash -c "sleep 8; roslaunch ego_planner sitl_ego_planner_basic.launch; exec bash"' \
 
 
--- Scripts/simulation/px4_gazebo_sitl_test/px4_sitl_outdoor.sh
+++ Scripts/simulation/px4_gazebo_sitl_test/px4_sitl_outdoor.sh
@@ -1,10 +1,6 @@
 # 脚本名称: px4_sitl_outdoor
 # 脚本描述: PX4 SITL Gazebo仿真环境测试
 
-# 启动PX4 SITL及Gazebo仿真环境
-roslaunch prometheus_gazebo sitl_outdoor_1uav.launch & sleep 5s;
-# 启动无人机控制 - uav_control_main
-roslaunch prometheus_uav_control uav_control_main_outdoor.launch
-
-
-
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_gazebo sitl_outdoor_1uav.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch prometheus_uav_control uav_control_main_outdoor.launch; exec bash"' \
