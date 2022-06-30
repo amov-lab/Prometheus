8d2188eb47b22a0966a4373a9f3e39f0e3c419c0
onx <betterming.wang@gmail.com>
2022-06-30 11:24:35 +0800

    update run sh

--- Scripts/simulation/tutorial_demo/autonomous_landing.sh
+++ Scripts/simulation/tutorial_demo/autonomous_landing.sh
@@ -2,9 +2,4 @@
 # 脚本描述：二维码识别降落
 # 相关文档：Modules/tutorial_demo/advanced/autonomous_landing/readme.md
 
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch prometheus_demo sitl_outdoor_1uav_autonomous_landing.launch; exec bash"' \
---tab -e 'bash -c "sleep 6; roslaunch prometheus_uav_control uav_control_main_outdoor.launch; exec bash"' \
---tab -e 'bash -c "sleep 7; roslaunch prometheus_demo landpad_datection.launch; exec bash"' \
---tab -e 'bash -c "sleep 8; roslaunch prometheus_demo autonomous_landing.launch; exec bash"' \
---tab -e 'bash -c "sleep 9; rqt_image_view; exec bash"' \
+roslaunch prometheus_demo autonomous_landing_all.launch
--- Scripts/simulation/tutorial_demo/yolov5_track.sh
+++ Scripts/simulation/tutorial_demo/yolov5_track.sh
@@ -2,10 +2,12 @@
 # 脚本描述： 使用yolov5对目标进行识别，然后点击识别框使用siamrpn进行目标跟踪(需要GPU，且配置按文档配置好环境)
 # 相关文档： Modules/tutorial_demo/advanced/yolov5_track/readme.md
 
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch prometheus_demo sitl_outdoor_1uav_yolov5_track.launch; exec bash"' \
---tab -e 'bash -c "sleep 6; roslaunch prometheus_uav_control uav_control_main_outdoor.launch; exec bash"' \
---tab -e 'bash -c "sleep 7; roslaunch prometheus_demo yolov5_detection.launch; exec bash"' \
---tab -e 'bash -c "sleep 8; python3 yolov5_trt_ros.py  --image_topic /prometheus/sensor/monocular_front/image_raw; exec bash"' \
---tab -e 'bash -c "sleep 9; roslaunch prometheus_demo yolov5_track.launch; exec bash"' \
---tab -e 'bash -c "sleep 10; rqt_image_view; exec bash"' \
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+if [ ! -d $SHELL_FOLDER/../../../Modules/object_detection_yolov5tensorrt ]; then
+    echo -e '\033[31m [ERROR] Please reading Modules/tutorial_demo/advanced/yolov5_track/readme.md to install \033[0m '
+    exit -1
+fi
+cd $SHELL_FOLDER/../../../Modules/object_detection_yolov5tensorrt
+gnome-terminal --tab -- python3 yolov5_trt_ros.py  --image_topic /prometheus/sensor/monocular_front/image_raw
+roslaunch prometheus_demo yolov5_track_all.launch
