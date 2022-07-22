bdf707105036eb0a21c8d981eeb9eb69b7924504
Yuhua <fatmoonqyp@126.com>
2022-07-22 12:54:43 +0800

    fix landing bug

--- Modules/uav_control/include/uav_controller.h
+++ Modules/uav_control/include/uav_controller.h
@@ -112,6 +112,7 @@ public:
     float Takeoff_height; // 默认起飞高度
     float Disarm_height;  // 自动上锁高度
     float Land_speed;     // 降落速度
+    bool set_landing_des;
 
     // 无人机状态量
     Eigen::Vector3d uav_pos;     // 无人机位置
