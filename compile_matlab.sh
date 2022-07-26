3ba1d4da7639d45c97fe6dcebf76962fb3ac802a
cgmasterss <1120809699@qq.com>
2022-07-26 11:18:55 +0800

    hover(0,0,1)

---
+++ compile_matlab.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 脚本名称: compile_control.sh
+# 脚本描述: 编译Prometheus控制模块
+
+# 编译基础模块
+catkin_make --source Modules/common --build build/common
+# 编译Gazebo仿真模块
+catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
+# 编译控制模块
+catkin_make --source Modules/uav_control --build build/uav_control
+# 编译demo模块
+catkin_make --source Modules/tutorial_demo --build build/tutorial_demo
+#编译matlab模块
+catkin_make --source Modules/matlab_bridge --build build/prometheus_matlab
