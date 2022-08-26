72ee6fbcded49a296580cc0a603bf9fffeea94d6
qi <fatmoonqyp@126.com>
2022-08-27 01:19:18 +0800

    a_star

--- Scripts/simulation/motion_planning/astar_with_2dlidar.sh
+++ Scripts/simulation/motion_planning/astar_with_2dlidar.sh
@@ -2,7 +2,7 @@
 # 脚本描述: 单个无人机的astar算法测试(局部点云)
 
 gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch prometheus_simulator_utils map_generator.launch; exec bash"' \
---tab -e 'bash -c "sleep 6; roslaunch prometheus_gazebo sitl_px4_indoor_sensor.launch uav_init_x:=-10; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_simulator_utils map_generator.launch map_generator_enable:=false; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch prometheus_gazebo sitl_indoor_1uav.launch gazebo_enable:=false uav1_init_x:=-10; exec bash"' \
 --tab -e 'bash -c "sleep 7; roslaunch prometheus_uav_control uav_control_main_indoor.launch; exec bash"' \
 --tab -e 'bash -c "sleep 8; roslaunch prometheus_global_planner sitl_global_planner_with_2dlidar.launch; exec bash"' \
