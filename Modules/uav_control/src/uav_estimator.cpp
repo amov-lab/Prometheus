ff54e997a62320457896489e87aebde47ba54b32
eason <473867143@qq.com>
2022-12-03 14:37:13 +0800

    update

--- Modules/uav_control/src/uav_estimator.cpp
+++ Modules/uav_control/src/uav_estimator.cpp
@@ -73,7 +73,7 @@ UAV_estimator::UAV_estimator(ros::NodeHandle &nh)
     else if (location_source == prometheus_msgs::UAVState::UWB)
     {
         // 【订阅】UWB
-        uwb_sub = nh.subscribe<prometheus_msgs::LinktrackNodeframe2>("/nlink_linktrack_nodeframe2", 10, &UAV_estimator::uwb_cb, this);
+        uwb_sub = nh.subscribe<prometheus_msgs::LinktrackNodeframe2>(uav_name + "/nlink_linktrack_nodeframe2", 10, &UAV_estimator::uwb_cb, this);
     }
     else
     {
