43f49d9eb002e766bba604ffb65d4eb9b92fa42a
amov-nuc <fatmoonqyp@126.com>
2023-03-24 23:09:44 +0800

    add octomap

--- GFKD_scripts/ego_planner_1uav_gfkd_octomap.sh
+++ GFKD_scripts/ego_planner_1uav_gfkd_octomap.sh
@@ -5,4 +5,4 @@ gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 5; roslaunch prometheus_gazebo gfkd_rviz.launch; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch prometheus_gazebo sitl_indoor_1uav_scan.launch; exec bash"' \
 --tab -e 'bash -c "sleep 7; roslaunch prometheus_uav_control uav_control_main_indoor.launch; exec bash"' \
---tab -e 'bash -c "sleep 8; roslaunch ego_planner sitl_ego_planner_basic.launch; exec bash"' \
+--tab -e 'bash -c "sleep 8; roslaunch ego_planner sitl_ego_planner_basic_octomap.launch; exec bash"' \
