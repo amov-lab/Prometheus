af477499cfde52cb34c52642ca239b71e06e6707
amov-nuc-gpu <fatmoonqyp@126.com>
2023-03-27 22:56:16 +0800

    fix realsense bug

--- compile_GFKD.sh
+++ compile_GFKD.sh
@@ -6,9 +6,8 @@
 catkin_make --source Modules/common --build build/common
 # 编译Gazebo仿真模块
 catkin_make --source Simulator/gazebo_simulator --build build/prometheus_gazebo
-catkin_make --source Simulator/realsense_ros_gazebo --build build/realsense_ros_gazebo
+catkin_make --source Simulator/realsense_gazebo_plugin --build build/realsense_gazebo_plugin
 catkin_make --source Simulator/velodyne_gazebo_plugins --build build/velodyne_gazebo_plugins
-
 # 编译控制模块
 catkin_make --source Modules/uav_control --build build/uav_control
 # 编译规划相关模块
