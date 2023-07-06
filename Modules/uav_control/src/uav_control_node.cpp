5ac8b61901a617b4d96ccc030907a6b9bf90c820
amov-nuc-gpu <fatmoonqyp@126.com>
2023-07-07 02:21:02 +0800

    update planning scripts

--- Modules/uav_control/src/uav_control_node.cpp
+++ Modules/uav_control/src/uav_control_node.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "uav_controller_node");
     ros::NodeHandle nh("~");
-    ros::Rate rate(50.0);
+    ros::Rate rate(100.0);
 
     signal(SIGINT, mySigintHandler);
     ros::Duration(1.0).sleep();
---
+++ Scripts/simulation/ego_planner/ego_planner_p450_d435i.sh
@@ -0,0 +1,8 @@
+# 脚本描述: 
+
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch prometheus_gazebo sitl_p450_d435i.launch uav1_init_x:=-0.0 uav1_init_y:=-0.0; exec bash"' \
+--tab -e 'bash -c "sleep 4; roslaunch prometheus_uav_control uav_control_main_outdoor.launch; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch prometheus_gazebo depth_to_octomap.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch ego_planner sitl_ego_planner_basic_octomap.launch; exec bash"' \
+
