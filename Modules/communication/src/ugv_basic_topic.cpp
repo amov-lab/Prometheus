00f7f79f191eed7c8a74bf924ea69b474e845cda
Zhang__Ling <a1544839693@163.com>
2023-08-25 16:08:17 +0800

    fix cluster id issues

--- Modules/communication/src/ugv_basic_topic.cpp
+++ Modules/communication/src/ugv_basic_topic.cpp
@@ -1,6 +1,6 @@
 #include "ugv_basic_topic.hpp"
 
-UGVBasic::UGVBasic(ros::NodeHandle &nh,int id,Communication *communication)
+UGVBasic::UGVBasic(ros::NodeHandle &nh, int id, Communication *communication)
 {
     this->communication_ = communication;
 
@@ -8,8 +8,12 @@ UGVBasic::UGVBasic(ros::NodeHandle &nh,int id,Communication *communication)
     nh.param<std::string>("ground_station_ip", udp_ip, "127.0.0.1");
     nh.param<std::string>("multicast_udp_ip", multicast_udp_ip, "224.0.0.88");
 
-    this->ugv_cmd_pub_ = nh.advertise<prometheus_msgs::UGVCommand>("/ugv" + to_string(robot_id) + "/prometheus/ugv_command", 1000);
-    this->ugv_state_sub_ = nh.subscribe("/ugv" + to_string(robot_id) + "/prometheus/ugv_state", 10, &UGVBasic::stateCb, this);
+    int offset;
+    nh.param<int>("swarm_num", offset, 0);
+    this->robot_id = id + offset;
+
+    this->ugv_cmd_pub_ = nh.advertise<prometheus_msgs::UGVCommand>("/ugv" + to_string(id) + "/prometheus/ugv_command", 1000);
+    this->ugv_state_sub_ = nh.subscribe("/ugv" + to_string(id) + "/prometheus/ugv_state", 10, &UGVBasic::stateCb, this);
 }
 
 UGVBasic::~UGVBasic()
@@ -23,7 +27,7 @@ void UGVBasic::ugvCmdPub(struct UGVCommand ugv_command)
     ugv_command_.header.stamp = ros::Time::now();
     ugv_command_.Command_ID = ugv_command.Command_ID;
     ugv_command_.Mode = ugv_command.Mode;
-    for(int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         ugv_command_.position_ref[i] = ugv_command.position_ref[i];
         ugv_command_.linear_vel[i] = ugv_command.linear_vel[i];
@@ -39,7 +43,7 @@ void UGVBasic::stateCb(const prometheus_msgs::UGVState::ConstPtr &msg)
     this->ugv_state_.secs = msg->header.stamp.sec;
     this->ugv_state_.nsecs = msg->header.stamp.nsec;
     this->ugv_state_.battery = msg->battery;
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         this->ugv_state_.position[i] = msg->position[i];
         this->ugv_state_.velocity[i] = msg->velocity[i];
@@ -49,8 +53,8 @@ void UGVBasic::stateCb(const prometheus_msgs::UGVState::ConstPtr &msg)
     this->ugv_state_.attitude_q.y = msg->attitude_q.y;
     this->ugv_state_.attitude_q.z = msg->attitude_q.z;
     this->ugv_state_.attitude_q.w = msg->attitude_q.w;
-    
-    this->communication_->sendMsgByUdp(this->communication_->encodeMsg(Send_Mode::UDP,ugv_state_),multicast_udp_ip);
+
+    this->communication_->sendMsgByUdp(this->communication_->encodeMsg(Send_Mode::UDP, ugv_state_, robot_id), multicast_udp_ip);
     setTimeStamp(msg->header.stamp.sec);
 }
 
