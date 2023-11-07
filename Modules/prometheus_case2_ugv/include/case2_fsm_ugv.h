f17f4e557edca95c35c4369d69f97fef1741e1ef
xiqiaoliuxie <3282676804@qq.com>
2023-11-07 20:33:14 +0800

    add astar_ugv

---
+++ Modules/prometheus_case2_ugv/include/case2_fsm_ugv.h
@@ -0,0 +1,210 @@
+#ifndef CASE2_FSM_UGV
+#define CASE2_FSM_UGV
+
+#include <ros/ros.h>
+#include <boost/format.hpp>
+
+#include <Eigen/Eigen>
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+
+#include <std_msgs/Bool.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+
+#include "prometheus_msgs/PositionReference.h"
+#include "prometheus_msgs/UGVState.h"
+#include "prometheus_msgs/UGVCommand.h"
+#include "prometheus_msgs/ArucoInfo.h"
+#include "prometheus_msgs/Case2Result.h"
+#include "prometheus_msgs/Case3Result.h"
+#include "prometheus_msgs/StationCommand.h"
+#include "prometheus_msgs/MultiDetectionInfo.h"
+
+#include "A_star.h"
+#include "occupy_map.h"
+#include "printf_utils.h"
+
+#include "angles/angles.h"
+#include <tf2/utils.h> // getYaw
+
+using namespace std;
+
+#define MIN_DIS 0.1
+
+namespace prometheus_case2_ugv
+{
+
+class Case2FSM_UGV
+{
+
+private:
+    // ros nh
+    ros::NodeHandle global_planner_nh;
+    // 【订阅】地面站指令
+    ros::Subscriber station_cmd_sub;
+    // 【订阅】目标点（手动模式）
+    ros::Subscriber goal_sub;
+    // 【订阅】无人车状态
+    ros::Subscriber ugv_state_sub;
+    // 【订阅】其他无人车odom
+    ros::Subscriber nei_odom_sub[21];
+    // 【订阅】传感器数据 - 全局点云、局部点云、Scan
+    ros::Subscriber Gpointcloud_sub;
+    ros::Subscriber Lpointcloud_sub;
+    ros::Subscriber laserscan_sub;
+    // 【订阅】识别结果
+    ros::Subscriber detection_sub, detection_sub_real;
+    // 【发布】控制指令
+    ros::Publisher command_pub;
+    // 【发布】规划路径
+    ros::Publisher path_cmd_pub;
+    // 【发布】检测结果至其他无人车、无人车、地面站
+    ros::Publisher case2_result_pub;
+    // 【定时器】主循环定时器、路径追踪定时器、目标追踪定时器
+    ros::Timer mainloop_timer, track_path_timer, object_tracking_timer, send_nei_odom_timer;
+    // 五种状态机
+    enum EXEC_STATE
+    {
+      INIT,
+      WAIT_GOAL,
+      PLAN,
+      PATH_TRACKING,
+      OBEJECT_TRACKING,
+      RETURN,
+      STOP
+    };
+    EXEC_STATE exec_state;
+    // 集群数量
+    int swarm_num_ugv;     
+    // 无人车名字                             
+    string ugv_name;    
+    bool follow_target;
+    float k_p,k_aoivd; 
+    // 是否仿真模式
+    bool sim_mode;    
+    // 无人车编号                         
+    int ugv_id;                                     
+    // 无人车高度
+    double ugv_height;
+    double depth;
+    double angle_y;
+    // 手动给定目标点模式 或 自动目标点模式
+    bool manual_mode;
+    // 传感器输入flag
+    int map_input_source;
+    // 路径重规划时间
+    double replan_time;
+    double track_frequency;
+    int counter_search;
+    // 其他无人车位置
+    nav_msgs::Odometry nei_odom[21];
+    Eigen::Vector3d odom_nei[21];
+    bool get_nei_odom[21];
+    // A星规划器
+    Astar::Ptr Astar_ptr;
+    // A星规划器状态
+    int astar_state;
+    // 无人车状态
+    prometheus_msgs::UGVState ugv_state;
+    // 地面站指令
+    prometheus_msgs::StationCommand station_cmd;
+    // 发布的控制指令
+    prometheus_msgs::UGVCommand Command_Now;
+    // 无人车里程计信息
+    nav_msgs::Odometry ugv_odom;
+    //yaw
+    float ugv_yaw;
+    // 规划得到的路径
+    nav_msgs::Path path_cmd;
+    // 路经点开始id
+    int start_point_index;
+    // 路经点总数
+    int Num_total_wp;
+    // 当前执行ID
+    int cur_id;
+    // 距离上一次重置，移动的距离（重规划路径时重置）
+    float distance_walked;
+    float yaw_ref;
+    // 上一次重置时，无人车的位置
+    Eigen::Vector3d ugv_pos_last;
+    // 规划初始状态及终端状态
+    Eigen::Vector3d start_pos, start_vel, start_acc, goal_pos, goal_vel;
+    // 返航位置
+    Eigen::Vector3d return_pos;
+    // 规划器状态
+    bool odom_ready;
+    bool ugv_ready;
+    bool sensor_ready;
+    bool station_ready;
+    bool get_goal; 
+    bool in_return_mode;
+    bool path_ok;
+    bool rotate_in_place;  // vinson add
+    bool start_move; // vinson add
+    bool get_target_pos;
+    // 目标位置
+    geometry_msgs::PoseStamped target_pos;
+    // 上一条轨迹开始时间
+    ros::Time tra_start_time;    
+
+    // 检测相关状态量
+    bool detected_by_myself;
+    bool detected_by_others;
+    
+    // 是否丢失目标
+    bool lost_object;
+    int num_count_vision_lost;
+    int num_count_vision_get;
+    // 目标位置
+    Eigen::Vector3d object_pos_body,object_pos_enu;
+    double dis_to_object;
+    // 检测范围
+    Eigen::Vector2d detection_range_x,detection_range_y;
+    
+    // 回调函数
+    void goal_cb(const geometry_msgs::PoseStampedConstPtr& msg);
+    void ugv_state_cb(const prometheus_msgs::UGVStateConstPtr &msg);
+    void Gpointcloud_cb(const sensor_msgs::PointCloud2ConstPtr &msg);
+    void Lpointcloud_cb(const sensor_msgs::PointCloud2ConstPtr &msg);
+    void laser_cb(const sensor_msgs::LaserScanConstPtr &msg);
+    void detection_cb(const prometheus_msgs::ArucoInfoConstPtr &msg);
+    void detection_cb_real(const prometheus_msgs::MultiDetectionInfoConstPtr &msg);
+    void cmd_cb(const prometheus_msgs::StationCommandConstPtr& msg);
+    void nei_odom_cb(const nav_msgs::Odometry::ConstPtr& odom, int id);
+
+    void send_nei_odom_cb(const ros::TimerEvent& e);
+    void mainloop_cb(const ros::TimerEvent& e);
+    void track_path_cb(const ros::TimerEvent& e);
+    void object_tracking_cb(const ros::TimerEvent& e);
+   
+    // 【获取当前时间函数】 单位：秒
+    float get_time_in_sec(const ros::Time& begin_time);
+    int get_start_point_id(void);
+    void printf_exec_state();
+
+    // vinson
+    // tool function
+    const int get_track_point_id();
+
+    
+public:
+    Case2FSM_UGV(/* args */)
+    {    
+    }
+    ~Case2FSM_UGV()
+    {
+    }
+
+    void init(ros::NodeHandle& nh);
+};
+
+}
+
+#endif
---
+++ Modules/prometheus_case2_ugv/include/occupy_map.h
@@ -0,0 +1,129 @@
+#ifndef _OCCUPY_MAP_H
+#define _OCCUPY_MAP_H
+
+#include <iostream>
+#include <algorithm>
+
+#include <ros/ros.h>
+#include <Eigen/Eigen>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <tf/transform_listener.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
+#include <sensor_msgs/LaserScan.h>
+#include <laser_geometry/laser_geometry.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
+#include <map>
+#include "printf_utils.h"
+using namespace std;
+
+namespace prometheus_case2_ugv
+{
+
+class Occupy_map
+{
+    public:
+        Occupy_map(){}
+        // 局部地图 滑窗 存储器
+        map<int,pcl::PointCloud<pcl::PointXYZ>> point_cloud_pair;
+        // 全局地图点云指针
+        pcl::PointCloud<pcl::PointXYZ>::Ptr global_point_cloud_map;
+        // 全局膨胀点云指针
+        pcl::PointCloud<pcl::PointXYZ>::Ptr global_ugv_pcl;
+        // 考虑变指针
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_inflate_vis_;
+        // 临时指针
+        pcl::PointCloud<pcl::PointXYZ>::Ptr input_point_cloud;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_ptr;
+        // 地图边界点云
+        pcl::PointCloud<pcl::PointXYZ> border;
+        // VoxelGrid过滤器用于下采样
+        pcl::VoxelGrid<pcl::PointXYZ> vg;
+        // OutlierRemoval用于去除离群值
+	    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+        // laserscan2pointcloud2 中间变量
+        sensor_msgs::PointCloud2 input_laser_scan;
+        // laserscan2pointcloud2 投影器
+        laser_geometry::LaserProjection projector_;
+        // 上一帧odom
+        double f_x, f_y, f_z, f_roll, f_pitch, f_yaw;
+        // 局部地图滑窗，指示器以及大小
+        int st_it, queue_size;
+        // flag：展示地图边界
+        bool show_border;
+        bool sim_mode;
+        // 地图是否占据容器， 从编程角度来讲，这就是地图变为单一序列化后的索引
+        std::vector<int> occupancy_buffer_;  // 0 is free, 1 is occupied
+        // 代价地图
+        std::vector<double> cost_map_;  // cost
+        // 地图分辨率
+        double resolution_, inv_resolution_;
+        string node_name;
+        // 膨胀参数
+        double inflate_;
+        double odom_inflate_;
+        // 地图原点,地图尺寸
+        Eigen::Vector3d origin_, map_size_3d_, min_range_, max_range_;
+        // 占据图尺寸 = 地图尺寸 / 分辨率
+        Eigen::Vector3i grid_size_;
+        int swarm_num_ugv;                                  // 集群数量
+        string ugv_name;                                // 无人机名字
+        int ugv_id;                                     // 无人机编号
+        bool has_global_point;
+        bool get_gpcl,get_lpcl,get_laser;
+        Eigen::Vector3d enum_p[100], enum_p_ugv[1000], enum_p_cost[1000];
+        int ifn;
+        int inflate_index, inflate_index_ugv, cost_index, cost_inflate;
+        // 发布点云用于rviz显示
+        ros::Publisher global_pcl_pub, inflate_pcl_pub;
+        ros::Timer pcl_pub;
+
+        double ugv_height;
+        //初始化
+        void init(ros::NodeHandle& nh);
+        // 地图更新函数 - 输入：全局点云
+        void map_update_gpcl(const sensor_msgs::PointCloud2ConstPtr & global_point);
+        // 工具函数：合并局部地图
+        void local_map_merge_odom(const nav_msgs::Odometry & odom);
+        void ugv_pcl_update(Eigen::Vector3d *input_ugv_odom, bool *get_ugv_odom);
+        // 地图更新函数 - 输入：局部点云
+        void map_update_lpcl(const sensor_msgs::PointCloud2ConstPtr & local_point, const nav_msgs::Odometry & odom);
+        // 地图更新函数 - 输入：二维激光雷达
+        void map_update_laser(const sensor_msgs::LaserScanConstPtr & local_point, const nav_msgs::Odometry & odom);
+        // 地图膨胀
+        void inflate_point_cloud(void);
+        // 判断当前点是否在地图内
+        bool isInMap(Eigen::Vector3d pos);
+        // 设置占据
+        void setOccupancy(Eigen::Vector3d &pos, int occ);
+        // 设置代价
+        void updateCostMap(Eigen::Vector3d &pos, double cost);
+        // 由位置计算索引
+        void posToIndex(Eigen::Vector3d &pos, Eigen::Vector3i &id);
+        // 由索引计算位置
+        void indexToPos(Eigen::Vector3i &id, Eigen::Vector3d &pos);
+        // 根据位置返回占据状态
+        int getOccupancy(Eigen::Vector3d &pos);
+        // 根据索引返回占据状态
+        int getOccupancy(Eigen::Vector3i &id);
+        // 根据索引返回代价
+        double getCost(Eigen::Vector3d &pos);
+        // 检查安全
+        bool check_safety(Eigen::Vector3d &pos, double check_distance/*, Eigen::Vector3d& map_point*/);
+        void pub_pcl_cb(const ros::TimerEvent& e);
+        // 定义该类的指针
+        typedef std::shared_ptr<Occupy_map> Ptr;
+};
+
+}
+
+#endif
