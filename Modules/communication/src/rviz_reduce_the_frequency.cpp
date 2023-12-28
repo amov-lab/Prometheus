58185ad7db6c995eff40bc265329feeccffbfb4e
Zhang__Ling <a1544839693@163.com>
2023-12-28 15:42:07 +0800

    add point cloud compressed encode

--- Modules/communication/src/rviz_reduce_the_frequency.cpp
+++ Modules/communication/src/rviz_reduce_the_frequency.cpp
@@ -23,7 +23,13 @@ ReduceTheFrequency::ReduceTheFrequency(ros::NodeHandle &nh)
 
     // 发布降低频率后的话题
     octomap_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/octomap_point_cloud_centers/reduce_the_frequency", 100);
+    octomap_compressed_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/octomap_point_cloud_centers/reduce_the_frequency/compressed", 100);
+    // 点云数据降低频率
     occupancy_inflate_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/uav" + std::to_string(drone_id_) + "_ego_planner_node/grid_map/occupancy_inflate/reduce_the_frequency", 100);
+    // 点云数据降低频率并过滤
+    occupancy_inflate_filtered_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/uav" + std::to_string(drone_id_) + "_ego_planner_node/grid_map/occupancy_inflate/reduce_the_frequency/filtered", 100);
+    // 点云数据降低频率并压缩
+    occupancy_inflate_compressed_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/uav" + std::to_string(drone_id_) + "_ego_planner_node/grid_map/occupancy_inflate/reduce_the_frequency/compressed", 100);
     scan_pub_ = nh.advertise<sensor_msgs::LaserScan>("/scan/reduce_the_frequency", 100);
     scan_filtered_pub_ = nh.advertise<sensor_msgs::LaserScan>("/scan_filtered/reduce_the_frequency", 100);
     trajectory_pub_ = nh.advertise<nav_msgs::Path>("/uav" + std::to_string(drone_id_) + "/prometheus/trajectory/reduce_the_frequency", 100);
@@ -52,12 +58,15 @@ void ReduceTheFrequency::send1000MS(const ros::TimerEvent &time_event)
     if(octomap_point_cloud_ready)
     {
         octomap_pub_.publish(octomap_point_cloud);
+        octomap_compressed_pub_.publish(octomap_compressed_point_cloud);
         octomap_point_cloud_ready = false;
     }
     usleep(100000);
     if(occupancy_inflate_ready)
     {
         occupancy_inflate_pub_.publish(occupancy_inflate_point_cloud);
+        occupancy_inflate_filtered_pub_.publish(occupancy_inflate_filtered_point_cloud);
+        occupancy_inflate_compressed_pub_.publish(occupancy_inflate_compressed_point_cloud);
         occupancy_inflate_ready = false;
     }
     usleep(100000);
@@ -110,9 +119,9 @@ void ReduceTheFrequency::send50MS(const ros::TimerEvent &time_event)
 void ReduceTheFrequency::octomapPointCloudCentersCb(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
     octomap_point_cloud = *msg;
+    octomap_compressed_point_cloud = compressed(*msg);
     octomap_point_cloud_ready = true;
 }
-
 
 void ReduceTheFrequency::trajectoryCb(const nav_msgs::Path::ConstPtr &msg)
 {
@@ -135,6 +144,8 @@ void ReduceTheFrequency::scanFilteredCb(const sensor_msgs::LaserScan::ConstPtr &
 void ReduceTheFrequency::occupancyInflateCb(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
     occupancy_inflate_point_cloud = *msg;
+    occupancy_inflate_filtered_point_cloud = filtered(*msg);
+    occupancy_inflate_compressed_point_cloud = compressed(*msg);
     occupancy_inflate_ready = true;
 }
 
@@ -164,6 +175,8 @@ void ReduceTheFrequency::tfCb(const tf2_msgs::TFMessage::ConstPtr &msg)
 void ReduceTheFrequency::UAVMeshCb(const visualization_msgs::Marker::ConstPtr &msg)
 {   
     uav_mesh = *msg;
+    uav_position_x = uav_mesh.pose.position.x;
+    uav_position_y = uav_mesh.pose.position.y;
     uav_mesh_ready = true;
 }
 
@@ -171,4 +184,61 @@ void ReduceTheFrequency::optimalListCb(const visualization_msgs::Marker::ConstPt
 {
     optimal_list = *msg;
     optimal_list_ready = true;
+}
+
+sensor_msgs::PointCloud2 ReduceTheFrequency::filtered(const sensor_msgs::PointCloud2 msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(msg, *cloud);
+
+    // 创建滤波器对象
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(cloud);
+    pass.setFilterFieldName("x");  // 根据需要选择过滤的字段
+    pass.setFilterLimits(-3 + uav_position_x , 3 + uav_position_x);  // 设置范围
+    pass.filter(*cloud);  // 执行滤波操作
+    pass.setFilterFieldName("y");  // 根据需要选择过滤的字段
+    pass.setFilterLimits(-3 + uav_position_y , 3 + uav_position_y);  // 设置范围
+    pass.filter(*cloud);  // 执行滤波操作
+
+    // 转换回 ROS 点云消息
+    sensor_msgs::PointCloud2 decompressed_msg;
+    pcl::toROSMsg(*cloud, decompressed_msg);
+    decompressed_msg.header = msg.header;
+
+    return decompressed_msg;
+}
+
+sensor_msgs::PointCloud2 ReduceTheFrequency::compressed(const sensor_msgs::PointCloud2 msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(msg, *cloud);
+
+    // 创建Octree压缩对象
+    pcl::io::OctreePointCloudCompression<pcl::PointXYZ> octree_compression;
+    octree_compression.setInputCloud(cloud);
+
+    // 压缩点云数据
+    std::stringstream compressed_data;
+    octree_compression.encodePointCloud(cloud,compressed_data);
+
+    // 获取压缩数据的大小
+    std::string compressed_str = compressed_data.str();
+    size_t compressed_size = compressed_str.size();
+
+    // 创建动态分配的内存并复制压缩数据
+    uint8_t* compressed_data_ptr = new uint8_t[compressed_size];
+    std::memcpy(compressed_data_ptr, compressed_str.data(), compressed_size);
+
+    // 创建压缩后的PointCloud2消息
+    sensor_msgs::PointCloud2 compressed_cloud_msg;
+    compressed_cloud_msg.header = msg.header;
+    compressed_cloud_msg.row_step = compressed_size;  // 压缩数据的大小作为行步长
+    compressed_cloud_msg.is_dense = true;
+    compressed_cloud_msg.data = std::vector<uint8_t>(compressed_data_ptr, compressed_data_ptr + compressed_size);
+
+    // 释放动态分配的内存
+    delete[] compressed_data_ptr;// 获取压缩数据的大小
+
+    return compressed_cloud_msg;
 }
