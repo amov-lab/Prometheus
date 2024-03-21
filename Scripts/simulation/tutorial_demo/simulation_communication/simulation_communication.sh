454b903aefc6900c17fedddf6e20c76925715955
Zhang__Ling <a1544839693@163.com>
2024-03-21 17:05:15 +0800

    modify shell

--- Scripts/simulation/tutorial_demo/simulation_communication/simulation_communication.sh
+++ Scripts/simulation/tutorial_demo/simulation_communication/simulation_communication.sh
@@ -3,3 +3,4 @@
 # 脚本描述: 该脚本为起飞&降落控制demo启动脚本,包含PX4 SITL,Gazebo仿真环境,无人机控制节点以及起飞&降落控制节点
 
 gnome-terminal --window -e 'bash -c "sleep 2; roslaunch prometheus_communication_bridge simulation_bridge.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; roslaunch rosbridge_server rosbridge_websocket.launch; exec bash"' \
