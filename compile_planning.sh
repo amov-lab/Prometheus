1a98b2e086eee2ed9d89c9222b6d090f64321869
why <1789330827@qq.com>
2024-09-25 09:34:27 +0800

    add fast lio to compile_planning.sh

--- compile_planning.sh
+++ compile_planning.sh
@@ -14,3 +14,5 @@ catkin_make --source Modules/uav_control --build build/uav_control
 catkin_make --source Modules/simulator_utils --build build/simulator_utils
 catkin_make --source Modules/ego_planner_swarm --build build/ego_planner_swarm
 catkin_make --source Modules/motion_planning --build build/motion_planning
+# 编译建图定位模块
+catkin_make --source Modules/FAST_LIO --build build/FAST_LIO
