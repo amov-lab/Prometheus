e8579771371ee89517431dcfd141946011887be5
why_123456 <1789330827@qq.com>
2024-10-25 18:17:16 +0800

    add airsim_simulator

---
+++ compile_airsim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 脚本名称: compile_airsim.sh
+# 脚本描述: 编译Prometheus airsim模块
+
+# 编译基础模块
+catkin_make --source Modules/common --build build/common
+# 编译控制模块
+catkin_make --source Modules/uav_control --build build/uav_control
+# 编译通信模块
+catkin_make --source Modules/communication --build build/communication
+# 编译AirSim仿真模块
+catkin_make --source Simulator/airsim_simulator --build build/prometheus_airsim
+
+
