6b1059f777944f6bae24799cef54b779a898fdf1
why <1789330827@qq.com>
2025-02-20 15:01:30 +0800

    add record odom.txt

--- Modules/FAST_LIO/src/laserMapping.cpp
+++ Modules/FAST_LIO/src/laserMapping.cpp
@@ -59,7 +59,7 @@
 #include <prometheus_msgs/LivoxCustomMsg.h>
 #include "preprocess.h"
 #include <ikd-Tree/ikd_Tree.h>
-
+#include <fstream>
 #define INIT_TIME           (0.1)
 #define LASER_POINT_COV     (0.001)
 #define MAXN                (720000)
@@ -596,6 +596,52 @@ void publish_odometry(const ros::Publisher & pubOdomAftMapped)
     odomAftMapped.child_frame_id = "body";
     odomAftMapped.header.stamp = ros::Time().fromSec(lidar_end_time);// ros::Time().fromSec(lidar_end_time);
     set_posestamp(odomAftMapped.pose);
+
+    // 提取四元数
+    tf::Quaternion Q(
+        odomAftMapped.pose.pose.orientation.x,
+        odomAftMapped.pose.pose.orientation.y,
+        odomAftMapped.pose.pose.orientation.z,
+        odomAftMapped.pose.pose.orientation.w
+    );
+    // 转换为欧拉角（Z-Y-X顺序，单位：弧度）
+    tf::Matrix3x3 m(Q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    // 若文件已经存在就删除
+    static std::ofstream outfile;
+    static bool first_run = true;
+    if (first_run) {
+        const std::string filename = "/home/amov/odometry_data.txt";
+        // 检查文件是否存在
+        std::ifstream exist_check(filename);
+        if (exist_check.good()) {
+            exist_check.close();
+            if (std::remove(filename.c_str()) != 0) {
+                ROS_ERROR("Failed to delete existing file: %s", filename.c_str());
+            }
+        }
+        // 创建新文件
+        outfile.open(filename, std::ios::out | std::ios::trunc); // 明确指定截断模式
+        if (!outfile) {
+            ROS_ERROR("Failed to create new file: %s", filename.c_str());
+            return;
+        }
+        outfile << "\xEF\xBB\xBF"; 
+        outfile << "# timestamp(sec) x(m) y(m) z(m) roll(rad) pitch(rad) yaw(rad)\n";
+        first_run = false;
+    }
+    // 数据写入
+    //std::cout << "数据写入" <<std::endl;
+    outfile << std::fixed << std::setprecision(6)
+            << odomAftMapped.header.stamp.toSec() << ","  
+            << odomAftMapped.pose.pose.position.x << ","
+            << odomAftMapped.pose.pose.position.y << ","
+            << odomAftMapped.pose.pose.position.z << ","
+            << roll << "," << pitch << "," << yaw << std::endl;  
+    // ROS_INFO("/Odometry.pose.x = %.3f\t pose.y =%.3f\t pose.z = =%.3f \n",odomAftMapped.pose.pose.position.x,odomAftMapped.pose.pose.position.y,odomAftMapped.pose.pose.position.z,
+    // "orientation.x = %.3f\t orientation.y = %.3f\t orientation.z = %.3f\t ",roll, pitch, yaw);
+    
     pubOdomAftMapped.publish(odomAftMapped);
     auto P = kf.get_P();
     for (int i = 0; i < 6; i ++)
