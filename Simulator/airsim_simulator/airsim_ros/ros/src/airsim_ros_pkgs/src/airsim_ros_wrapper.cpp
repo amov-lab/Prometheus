f762ebb0e46524c6b6434373aeeeffb39377164b
XxM0924 <975593243@qq.com>
2025-03-20 18:48:29 +0800

    fix airsim disarm add depth ego

--- Simulator/airsim_simulator/airsim_ros/ros/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
+++ Simulator/airsim_simulator/airsim_ros/ros/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
@@ -75,7 +75,7 @@ void AirsimROSWrapper::initialize_airsim()
 
         for (const auto& vehicle_name_ptr_pair : vehicle_name_ptr_map_) {
             airsim_client_->enableApiControl(true, vehicle_name_ptr_pair.first); // todo expose as rosservice?
-            airsim_client_->armDisarm(true, vehicle_name_ptr_pair.first); // todo exposes as rosservice?
+            //airsim_client_->armDisarm(true, vehicle_name_ptr_pair.first); // todo exposes as rosservice?
         }
 
         origin_geo_point_ = airsim_client_->getHomeGeoPoint("");
